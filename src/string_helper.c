bb13665ea64aaa25b711f6649970b75d0eb5e607
angel <jeander4@ncsu.edu>
2013-03-19 08:39:09 -0400

    implemented string copy functionality

--- src/string_helper.c
+++ src/string_helper.c
@@ -6,7 +6,17 @@
 char *
 copy_string( char * str )
 {
-  return NULL;
+  if( str == NULL )
+    return NULL;
+  
+  size_t length = strlen( str );
+  char * copy = malloc( sizeof( char ) * length + 1 );
+  if( copy == NULL )
+    return NULL;
+  
+  strncpy( copy, str, length );
+  copy[length] = '\0';
+  return copy;
 }
 
 // returns 0 if the string contains anything other than whitespace
