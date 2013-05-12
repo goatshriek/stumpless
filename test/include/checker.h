75241b990263888186736a9ebc5666005d26c208
Joel <jeander4@ncsu.edu>
2013-05-12 03:41:44 +0100

    added string checking test helper

--- test/include/checker.h
+++ test/include/checker.h
@@ -4,11 +4,20 @@
 #include <stdlib.h>
 
 #define FAIL_IF_NOT_NULL( value, message )                                     \
-if( value != NULL )                                                            \
-  return message;
+if( (value) != NULL ){                                                         \
+  return (message);                                                            \
+}
 
 #define FAIL_IF_NULL( value, message )                                         \
-if( value == NULL )                                                            \
-  return message;
+if( (value) == NULL ){                                                         \
+  return (message);                                                            \
+}
+
+
+#define STRINGS_EQUAL( expected, actual, message )                             \
+if( strcmp( (expected), (actual) ) != 0 ){                                     \
+  printf( "Expected: %s, Instead was: %s\n" );                                 \
+  return (message);                                                            \
+}
 
 #endif
