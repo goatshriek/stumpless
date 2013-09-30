5972d4860d5ec1743c3e40e81a439d8c0f3f21a7
Joel <jeander4@ncsu.edu>
2013-09-30 18:14:45 +0000

    added function prototypes

--- include/private/dictionary.h
+++ include/private/dictionary.h
@@ -1,6 +1,24 @@
 #ifndef STUMPLESS_PRIVATE_DICTIONARY_H
 #define STUMPLESS_PRIVATE_DICIONTARY_H
 
+Status *
+AddValueToDictionary
+( Dictionary *, const char *, const void * );
 
+void
+DestroyDictionary
+( Dictionary * );
+
+void *
+GetValueFromDictionary
+( Dictionary *, const char * );
+
+Dictionary *
+NewDictionary
+();
+
+Status *
+RemoveValueFromDictionary
+( Dictionary *, const char * );
 
 #endif
---
+++ src/dictionary.c
@@ -0,0 +1,36 @@
+#include <stdlib.h>
+
+Status *
+AddValueToDictionary
+( Dictionary * dictionary, const char * key, const void * value )
+{
+  return NULL;
+}
+
+void
+DestroyDictionary
+( Dictionary * dictionary )
+{
+  return;
+}
+
+void *
+GetValueFromDictionary
+( Dictionary * dictionary, const char * key )
+{
+  return NULL;
+}
+
+Dictionary *
+NewDictionary
+()
+{
+  return NULL;
+}
+
+Status *
+RemoveValueFromDictionary
+( Dictionary * dictionary, const char * key )
+{
+  return NULL;
+}
