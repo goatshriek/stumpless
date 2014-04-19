fd1aea224a37f2fcf3db7d4a87ae393b5540e609
joel <jeander4@ncsu.edu>
2014-04-19 02:27:05 +0000

    added basic wrapper generation files

---
+++ src/stumpless/wrapper_generator.c
@@ -0,0 +1,18 @@
+int
+main
+( void )
+{
+  return 0;
+}
+
+// read type definition file
+
+// generate private typedef file
+
+// iterate through non-stumpless src files
+
+// generate stumpless src files
+
+// generate stumpless include files
+
+// generate stumpless.h file
