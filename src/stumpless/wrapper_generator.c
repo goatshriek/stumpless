2585f17065ea2be407a739ca628f6bc1d04b5cf2
joel <jeander4@ncsu.edu>
2014-04-24 22:37:22 +0000

    added beginning of wrapper generator program

--- src/stumpless/wrapper_generator.c
+++ src/stumpless/wrapper_generator.c
@@ -1,18 +1,62 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+const char * prefix;
+static char *types[];
+
+int ReadTypes( void );
+int GenerateTypedefs( void );
+int GenerateStumplessHeader( void );
+
 int
 main
+( int argc, char *argv[] )
+{
+  if( argc < 1 )
+    prefix = "Stumpless";
+  else
+    prefix = argv[0];
+  
+  if( !ReadTypes() )
+    return EXIT_FAILURE;
+  
+  if( !GenerateTypedefs() )
+    return EXIT_FAILURE;
+  
+  if( !GenerateStumplessHeader() )
+    return EXIT_FAILURE;
+  
+  return EXIT_SUCCESS;
+}
+
+int
+ReadTypes
+( void )
+{
+  // todo read in full filename from config file if possible
+  const char *type_filename = "include/stumpless/type.h";
+  
+  FILE *type_file = fopen( type_filename, "r" );
+  if( !type_file )
+    return NULL;
+  
+  return 0;
+}
+
+int
+GenerateTypedefs
 ( void )
 {
   return 0;
 }
 
-// read type definition file
-
-// generate private typedef file
-
 // iterate through non-stumpless src files
-
 // generate stumpless src files
-
 // generate stumpless include files
 
-// generate stumpless.h file
+int
+GenerateStumplessHeader
+( void )
+{
+  return 0;
+}
