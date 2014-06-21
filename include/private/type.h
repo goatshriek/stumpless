c34f07beef05bb425bbcd3ba4acf3f3cf526a265
angel <jeander4@ncsu.edu>
2014-06-21 17:13:32 -0400

    changed private type declarations to match public structure

--- include/private/type.h
+++ include/private/type.h
@@ -3,22 +3,6 @@
 
 #include "stumpless/type.h"
 
-struct Dimension;
-struct List;
-struct ListConstIterator;
-struct ListConstReverseIterator;
-struct ListIterator;
-struct ListReverseIterator;
-struct Stack;
-struct Tree;
-
-typedef struct Dimension Dimension;
-typedef struct List List;
-typedef struct ListConstIterator ListConstIterator;
-typedef struct ListConstReverseIterator ListConstReverseIterator;
-typedef struct ListIterator ListIterator;
-typedef struct ListReverseIterator ListReverseIterator;
-typedef struct Stack Stack;
-typedef struct Tree Tree;
+#include "private/type/declaration.h"
 
 #endif
