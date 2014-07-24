79a72e15e8f6dc18a722e896a4407cec63b98a5a
Joel Anderson <jeander4@ncsu.edu>
2014-07-24 16:34:31 -0400

    add logger getter function stubs

---
+++ include/private/logger/getter.h
@@ -0,0 +1,10 @@
+#ifndef __STUMPLESS_PRIVATE_LOGGER_GETTER_H
+#define __STUMPLESS_PRIVATE_LOGGER_GETTER_H
+
+#include "private/type.h"
+
+Logger *
+GetConsoleLogger
+( void );
+
+#endif
---
+++ src/logger/getter.c
@@ -0,0 +1,10 @@
+#include <stdlib.h>
+
+#include "private/getter.h"
+
+Logger *
+GetConsoleLogger
+( void )
+{
+  return NULL;
+}
