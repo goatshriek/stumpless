e5de2f7b0ec22fad6783aaffc4a181f4fa43f45f
Joel Anderson <jeander4@ncsu.edu>
2014-11-24 19:37:48 -0500

    update public include file

--- include/stumpless.h
+++ include/stumpless.h
@@ -3,6 +3,10 @@
 
 #include <stumpless/config.h>
 #include <stumpless/event.h>
+#include <stumpless/logger.h>
+#include <stumpless/record.h>
+#include <stumpless/record_attribute.h>
+#include <stumpless/target.h>
 #include <stumpless/type.h>
 #include <stumpless/value.h>
 
