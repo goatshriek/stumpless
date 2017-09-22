dcad22aae8a59f02d1b61b746228375604488df3
Joel Anderson <jeander@vt.edu>
2017-09-21 20:50:18 -0400

    add headers from stumplessd

--- headers_check.pl
+++ headers_check.pl
@@ -1,1 +1,13 @@
-memcpy : string.h
+my %manifest = (
+  "memcpy" => "string.h",
+  "printf" => "stdio.h",
+  "socklen_t" => "sys/socket.h",
+  "recvfrom" => "sys/socket.h",
+  "perror" => "stdio.h",
+  "ssize_t" => "sys/un.h",
+  "EXIT_FAILURE" => "stdlib.h",
+  "EXIT_SUCCESS" => "stdlib.h",
+  "NULL" => "stdlib.h",
+  "struct sockaddr" => "sys/socket.h",
+  "struct sockaddr_un" => "sys/un.h"
+)
