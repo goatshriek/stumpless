fc1daa8256bd27f358e95db6b6fb982390345243
Joel Anderson <jeander@vt.edu>
2017-11-14 17:26:08 -0500

    update Makefile

--- src/error.c
+++ src/error.c
@@ -16,7 +16,9 @@
  * You should have received a copy of the GNU General Public License along with
  * Stumpless.  If not, see <http://www.gnu.org/licenses/>.
  */
- 
+
+#include <stddef.h>
+#include <stdlib.h>
 #include <stumpless/error.h>
 
 static struct stumpless_error *last_error=NULL;
