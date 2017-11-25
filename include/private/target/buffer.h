e010b84b74dc87d7407bfc807dce2383d8e7657d
Joel Anderson <jeander@vt.edu>
2017-11-25 10:14:34 -0500

    add buffer target headers

---
+++ include/private/target/buffer.h
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2017, Joel Anderson.
+ * All Rights Reserved.
+ *
+ * This file is part of Stumpless.
+ * 
+ * Stumpless is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option)
+ * any later version.
+ * 
+ * Stumpless is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along with
+ * Stumpless.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef __STUMPLESS_PRIVATE_TARGET_BUFFER_H
+#define __STUMPLESS_PRIVATE_TARGET_BUFFER_H
+
+#include <stddef.h> 
+#include <stumpless/target.h>
+ 
+struct buffer_target {
+  char *buffer;
+  size_t size;
+  int local_socket;
+};
+
+void destroy_buffer_target(struct buffer_target *target);
+struct buffer_target *new_buffer_target(const char *buffer, size_t size);
+ssize_t sendto_buffer_target(const struct stumpless_target *target, const char *msg);
+
+#endif /* __STUMPLESS_PRIVATE_TARGET_BUFFER_H */
---
+++ include/stumpless/target/buffer.h
@@ -0,0 +1,37 @@
+/*
+ * Copyright 2017, Joel Anderson.
+ * All Rights Reserved.
+ *
+ * This file is part of Stumpless.
+ * 
+ * Stumpless is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option)
+ * any later version.
+ * 
+ * Stumpless is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along with
+ * Stumpless.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef __STUMPLESS_TARGET_BUFFER_H
+#define __STUMPLESS_TARGET_BUFFER_H
+
+#include <stddef.h>
+#include <stumpless/target.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void stumpless_close_buffer_target(struct stumpless_target *target);
+struct stumpless_target *stumpless_open_buffer_target(char *buffer, size_t size, int options, int facility);
+
+#ifdef __cplusplus
+} /* extern "C" */
+#endif
+
+#endif /* __STUMPLESS_TARGET_BUFFER_H */
