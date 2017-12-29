8dac99fa8c71edb5a6c05432257b282f86a5df9c
Joel Anderson <joelanderson333@gmail.com>
2017-12-29 12:19:31 -0500

    document subgroups in RFC 5424 regex

--- include/test/function.h
+++ include/test/function.h
@@ -33,10 +33,38 @@
  *   data is not detected
  * - the values in MSG are not checked for encoding differences, for example
  *   the BOM is not detected
- * todo document with parenthesis groups align to which elements in the RFC
+ *
+ * This expression will also populate several subgroups on the message, assuming
+ * that there is a match. A description of each of these subgroups ordered by
+ * their index is included below, using names from RFC 5424 where appropriate.
+ * For optional components, the subgroup will of course be empty if the item was
+ * absent in the message.
+ * 0  - SYSLOG-MSG (the entire message)
+ * 1  - PRIVAL
+ * 2  - VERSION
+ * 3  - TIMESTAMP
+ * 4  - FULL-DATE "T" FULL-YEAR if TIMESTAMP was not NILVALUE
+ * 5  - TIME-SECFRAC if it was provided
+ * 6  - TIME-OFFSET
+ * 7  - TIME-NUMOFFSET if provided (that is, if TIME-OFFSET was not "Z")
+ * 8  - The "+" or "-" character in TIME-NUMOFFSET if it was provided
+ * 9  - HOSTNAME
+ * 10 - HOSTNAME if it was not NILVALUE
+ * 11 - APP-NAME
+ * 12 - APP-NAME if it was not NILVALUE
+ * 13 - PROCID
+ * 14 - PROCID if it was not NILVALUE
+ * 15 - MSGID
+ * 16 - MSGID if it was not NILVALUE
+ * 17 - STRUCTURED-DATA
+ * 18 - 1*SD-ELEMENT if STRUCTURED-DATA was not NILVALUE
+ * 19 - 1*SD-ELEMENT IF STRUCTURED-DATA was not NILVALUE
+ * 20 - the last SD-ID in the message
+ * 21 - the last PARAM-NAME="PARAM-VALUE" in the message
+ * 22 - MSG (the log message itself)
  */
-#define RFC_5424_REGEX_STRING "^<\\d{1,3}>"                          /* PRI */ \
-                              "[1-9]\\d{0,2}"                     /* HEADER */ \
+#define RFC_5424_REGEX_STRING "^<(\\d{1,3})>"                        /* PRI */ \
+                              "([1-9]\\d{0,2})"                  /* VERSION */ \
                               " "                                     /* SP */ \
                               "(-|("                           /* TIMESTAMP */ \
                               "\\d{4}-\\d{2}-\\d{2}"           /* FULL-DATE */ \
@@ -58,12 +86,12 @@
                               " "                                     /* SP */ \
                               "(-|(("                    /* STRUCTURED-DATA */ \
                               "\\["                           /* SD-ELEMENT */ \
-                              "[!#-<>-\\\\\\^-~]{1,32}"            /* SD-ID */ \
+                              "([!#-<>-\\\\\\^-~]{1,32})"          /* SD-ID */ \
 			      "( [!#-<>-\\\\\\^-~]{1,32}"     /* PARAM-NAME */ \
-                              "=\".*\")*"                    /* PARAM-VALUE */ \
+                              "=\"[^\\]]*\")*"               /* PARAM-VALUE */ \
 			      "\\]"                           /* SD-ELEMENT */ \
                               ")+))"                     /* STRUCTURED-DATA */ \
                               " "                                     /* SP */ \
-                              ".*$"                                  /* MSG */
+                              "(.*)$"                                /* MSG */
 
 #endif /* __STUMPLESS_TEST_FUNCTION_H */
