3c71b22134d53e12bef6f8da663fdf296f369dcc
Joel Anderson <joelanderson333@gmail.com>
2017-12-30 12:05:29 -0500

    add checks for version, year, month, and day

--- include/test/function/rfc5424.hpp
+++ include/test/function/rfc5424.hpp
@@ -27,7 +27,8 @@
  * unchecked:
  * - the range of the PRIVAL is not validated, only that it is a number between
  *   one and three digits
- * - month and day values are not checked for sanit (for example, the 30th of
+ * - VERSION is not specifically checked to be "1"
+ * - month and day values are not checked for sanity (for example, the 30th of
  *   february will not be rejected
  * - proper escaping of characters in the PARAM-VALUE elements within structured
  *   data is not detected
@@ -38,36 +39,41 @@
  * that there is a match. A description of each of these subgroups ordered by
  * their index is included below, using names from RFC 5424 where appropriate.
  * For optional components, the subgroup will of course be empty if the item was
- * absent in the message.
+ * absent in the message. For subggroups that are always a component of the RFC
+ * grammar, there is a #define of the form RFC_5424_xxx_MATCH_INDEX, for example
+ * RFC_5424_HOSTNAME_MATCH_INDEX.
  * 0  - SYSLOG-MSG (the entire message)
  * 1  - PRIVAL
  * 2  - VERSION
  * 3  - TIMESTAMP
  * 4  - FULL-DATE "T" FULL-YEAR if TIMESTAMP was not NILVALUE
- * 5  - TIME-SECFRAC if it was provided
- * 6  - TIME-OFFSET
- * 7  - TIME-NUMOFFSET if provided (that is, if TIME-OFFSET was not "Z")
- * 8  - The "+" or "-" character in TIME-NUMOFFSET if it was provided
- * 9  - HOSTNAME
- * 10 - HOSTNAME if it was not NILVALUE
- * 11 - APP-NAME
- * 12 - APP-NAME if it was not NILVALUE
- * 13 - PROCID
- * 14 - PROCID if it was not NILVALUE
- * 15 - MSGID
- * 16 - MSGID if it was not NILVALUE
- * 17 - STRUCTURED-DATA
- * 18 - 1*SD-ELEMENT if STRUCTURED-DATA was not NILVALUE
- * 19 - 1*SD-ELEMENT IF STRUCTURED-DATA was not NILVALUE
- * 20 - the first SD-ID in the message
- * 21 - everything from the first PARAM-NAME to the last "PARAM-VALUE"
- * 22 - MSG (the log message itself)
+ * 5  - DATE-FULLYEAR
+ * 6  - DATE-MONTH
+ * 7  - DATE-MDAY
+ * 8  - TIME-SECFRAC if it was provided
+ * 9  - TIME-OFFSET
+ * 10 - TIME-NUMOFFSET if provided (that is, if TIME-OFFSET was not "Z")
+ * 11 - The "+" or "-" character in TIME-NUMOFFSET if it was provided
+ * 12 - HOSTNAME
+ * 13 - HOSTNAME if it was not NILVALUE
+ * 14 - APP-NAME
+ * 15 - APP-NAME if it was not NILVALUE
+ * 16 - PROCID
+ * 17 - PROCID if it was not NILVALUE
+ * 18 - MSGID
+ * 19 - MSGID if it was not NILVALUE
+ * 20 - STRUCTURED-DATA
+ * 21 - 1*SD-ELEMENT if STRUCTURED-DATA was not NILVALUE
+ * 22 - 1*SD-ELEMENT IF STRUCTURED-DATA was not NILVALUE
+ * 23 - the first SD-ID in the message
+ * 24 - everything from the first PARAM-NAME to the last "PARAM-VALUE"
+ * 25 - MSG (the log message itself)
  */
 #define RFC_5424_REGEX_STRING "^<(\\d{1,3})>"                        /* PRI */ \
                               "([1-9]\\d{0,2})"                  /* VERSION */ \
                               " "                                     /* SP */ \
                               "(-|("                           /* TIMESTAMP */ \
-                              "\\d{4}-\\d{2}-\\d{2}"           /* FULL-DATE */ \
+                              "(\\d{4})-(\\d{2})-(\\d{2})"     /* FULL-DATE */ \
 			      "T"                                    /* "T" */ \
                               "\\d{2}:\\d{2}:\\d{2}"        /* PARTIAL-TIME */ \
 			      "(\\.\\d{1,6})?"              /* TIME-SECFRAC */ \
@@ -93,6 +99,26 @@
                               ")+))"                     /* STRUCTURED-DATA */ \
                               " "                                     /* SP */ \
                               "(.*)$"                                /* MSG */
+
+#define RFC_5424_SYSLOG_MSG_MATCH_INDEX 0
+#define RFC_5424_PRIVAL_MATCH_INDEX 1
+#define RFC_5424_VERSION_MATCH_INDEX 2
+#define RFC_5424_TIMESTAMP_MATCH_INDEX 3
+#define RFC_5424_DATE_FULLYEAR_MATCH_INDEX 5
+#define RFC_5424_DATE_MONTH_MATCH_INDEX 6
+#define RFC_5424_DATE_MDAY_MATCH_INDEX 7
+#define RFC_5424_TIME_SECFRAC_MATCH_INDEX 8
+#define RFC_5424_TIME_OFFSET_MATCH_INDEX 9
+#define RFC_5424_TIME_NUMOFFSET_MATCH_INDEX 10
+#define RFC_5424_HOSTNAME_MATCH_INDEX 12
+#define RFC_5424_APP_NAME_MATCH_INDEX 14
+#define RFC_5424_PROCID_MATCH_INDEX 16
+#define RFC_5424_MSGID_MATCH_INDEX 18
+#define RFC_5424_STRUCTURED_DATA_MATCH_INDEX 20
+#define RFC_5424_MSG_MATCH_INDEX 25
+
+#define RFC_5424_PRIVAL_MIN 0
+#define RFC_5424_PRIVAL_MAX 191
 
 void TestRFC5424Compliance(const char *syslog_msg);
 
--- test/function/rfc5424.cpp
+++ test/function/rfc5424.cpp
@@ -22,15 +22,50 @@
 #include "test/function/rfc5424.hpp"
 
 void TestRFC5424Compliance(const char *syslog_msg){
-  int prival, i=0;
+  int prival, year, month, day, i=0;
   std::cmatch matches;
   std::regex rfcRegex(RFC_5424_REGEX_STRING);
 
   ASSERT_TRUE(std::regex_match(syslog_msg, matches, rfcRegex));
 
   prival = std::stoi(matches[1]);
-  EXPECT_GE(prival, 0);
-  EXPECT_LE(prival, 191);
+  EXPECT_GE(prival, RFC_5424_PRIVAL_MIN);
+  EXPECT_LE(prival, RFC_5424_PRIVAL_MAX);
+ 
+  EXPECT_EQ(matches[2], "1");
+
+  year = std::stoi(matches[RFC_5424_DATE_FULLYEAR_MATCH_INDEX]);
+  ASSERT_GE(year, 0);
+
+  month = std::stoi(matches[RFC_5424_DATE_MONTH_MATCH_INDEX]);
+  day = std::stoi(matches[RFC_5424_DATE_MDAY_MATCH_INDEX]);
+  EXPECT_GE(day, 1);
+  switch(month){
+    case 1:
+    case 3:
+    case 5:
+    case 7:
+    case 9:
+    case 11:
+      EXPECT_LE(day, 31);
+      break;
+    case 4:
+    case 6:
+    case 8:
+    case 10:
+    case 12:
+      EXPECT_LE(day, 30);
+      break;
+    case 2:
+      if(month % 4 == 0){
+        EXPECT_LE(day, 29);
+      } else {
+        EXPECT_LE(day, 28);
+      }
+      break;
+    default:
+      FAIL() << "DATE-MONTH was not a value between 1 and 12";
+  }
   
   for(auto m=matches.begin(); m != matches.end(); ++m){
     std::cout << "match " << i++ << ": " << *m << "\n";
