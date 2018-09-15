72afe3f64e6073543bf546b07aed492db293e2a5
Joel Anderson <joelanderson333@gmail.com>
2018-09-15 15:26:47 -0400

    move performance test definition to macro

--- tools/cmake/test.cmake
+++ tools/cmake/test.cmake
@@ -1,27 +1,54 @@
 macro(add_function_test name)
-  list(APPEND STUMPLESS_FUNCTION_TESTS ${name})
+  list(APPEND STUMPLESS_FUNCTION_TEST_TARGETS function-test-${name})
 
-  add_executable(${name}
+  add_executable(function-test-${name}
     EXCLUDE_FROM_ALL
     ${ARGN}
   )
 
-  set_target_properties(${name}
-    PROPERTIES COMPILE_FLAGS "-std=c++11"
+  set_target_properties(function-test-${name}
+    PROPERTIES
+    OUTPUT_NAME function-test-${name}
+    COMPILE_FLAGS "-std=c++11"
   )
 
-  target_link_libraries(${name}
+  target_link_libraries(function-test-${name}
     stumpless
     libgtest
   )
 
-  target_include_directories(${name}
+  target_include_directories(function-test-${name}
     PRIVATE
     ${PROJECT_SOURCE_DIR}/include
     ${CMAKE_BINARY_DIR}/include
   )
 
   add_test(NAME ${name}
-    COMMAND ${name}
+    COMMAND function-test-${name}
   )
 endmacro(add_function_test)
+
+macro(add_performance_test name)
+  list(APPEND STUMPLESS_PERFORMANCE_TEST_TARGETS performance-test-${name})
+
+  add_executable(performance-test-${name}
+    EXCLUDE_FROM_ALL
+    ${ARGN}
+  )
+
+  target_link_libraries(performance-test-${name}
+    stumpless
+    libbenchmark
+  )
+
+  set_target_properties(performance-test-${name}
+    PROPERTIES
+    OUTPUT_NAME performance-test-${name}
+  )
+
+  target_include_directories(performance-test-${name}
+    PRIVATE
+    ${PROJECT_SOURCE_DIR}/include
+    ${CMAKE_BINARY_DIR}/include
+  )
+endmacro(add_performance_test)
