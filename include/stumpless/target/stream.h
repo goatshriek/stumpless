c0436a18d380a129ba9d03688301d9519650a50e
Joel Anderson <joelanderson333@gmail.com>
2018-11-04 16:46:02 -0500

    add documentation for public stream functions

--- include/stumpless/target/stream.h
+++ include/stumpless/target/stream.h
@@ -16,6 +16,10 @@
  * limitations under the License.
  */
 
+/** @file
+ * Functions for working with stream targets (that is, FILE pointers).
+ */
+
 #ifndef __STUMPLESS_TARGET_STREAM_H
 #  define __STUMPLESS_TARGET_STREAM_H
 
@@ -26,9 +30,42 @@
 extern "C" {
 #  endif
 
+/**
+ * Closes a stream target.
+ *
+ * This function does NOT close the stream associated with the target. It does
+ * destroy all memory allocated for the target, including the target struct
+ * itself.
+ *
+ * @param target The stream target to close.
+ */
 void
 stumpless_close_stream_target( struct stumpless_target *target );
 
+/**
+ * Opens a stream target for the given stream.
+ *
+ * Events logged to this target will be separated by a newline, just as with a
+ * file target. In fact, a stream target is equivalent to a file target with the
+ * difference being that the file must be opened and closed by the caller.
+ *
+ * A stream target can also be useful for logging to other streams, such as
+ * stdout and stderr, which is not possible with a file target.
+ *
+ * @param name The name of the target to open. This is only used for
+ * identification of the target.
+ *
+ * @param stream The stream to log events to.
+ *
+ * @param options The options to use for the logging target. This is a bit-wise
+ * or of one or more \c STUMPLESS_OPTION values.
+ *
+ * @param default_facility The facility code to use for entries that do not have
+ * a facility value set on them.
+ *
+ * @return The opened target if no error is encountered. In the event of an
+ * error, NULL is returned and an error code is set appropriately.
+ */
 struct stumpless_target *
 stumpless_open_stream_target( const char *name,
                               FILE *stream,
