8b96c254df6d0f0eba9a951715156f06f4894d55
Joel Anderson <joelanderson333@gmail.com>
2018-11-06 20:08:04 -0500

    remove unnecessary test code

--- test/function/target/stream.cpp
+++ test/function/target/stream.cpp
@@ -200,16 +200,9 @@ namespace {
   TEST( StreamTargetStderrTest, NullName ) {
     struct stumpless_target *target;
     struct stumpless_error *error;
-    const char *filename = "null-name.log";
-    FILE *stream;
-
-    stream = fopen( filename, "w+" );
-    ASSERT_TRUE( stream != NULL );
 
     target = stumpless_open_stderr_target( NULL, 0, 0 );
     EXPECT_TRUE( target == NULL );
-
-    fclose( stream );
 
     error = stumpless_get_error(  );
     ASSERT_TRUE( error != NULL );
@@ -219,16 +212,9 @@ namespace {
   TEST( StreamTargetStdoutTest, NullName ) {
     struct stumpless_target *target;
     struct stumpless_error *error;
-    const char *filename = "null-name.log";
-    FILE *stream;
-
-    stream = fopen( filename, "w+" );
-    ASSERT_TRUE( stream != NULL );
 
     target = stumpless_open_stdout_target( NULL, 0, 0 );
     EXPECT_TRUE( target == NULL );
-
-    fclose( stream );
 
     error = stumpless_get_error(  );
     ASSERT_TRUE( error != NULL );
