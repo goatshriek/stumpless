19554ff469d21addb2d26565a45bfc46482757f9
Joel Anderson <joelanderson333@gmail.com>
2018-11-11 17:50:14 -0500

    remove other reference to PF_INET

--- src/config/have_sys_socket.c
+++ src/config/have_sys_socket.c
@@ -67,7 +67,7 @@ sys_socket_open_udp4_target( struct udp4_details *details,
 
   memset( &details->target_addr, '\0', sizeof( struct sockaddr_storage ) );
   cast_addr_in = ( struct sockaddr_in * ) &details->target_addr;
-  cast_addr_in->sin_family = AF_INET;
+  cast_addr_in->sin_family = PF_INET;
   inet_pton( PF_INET, destination, &cast_addr_in->sin_addr.s_addr );
   cast_addr_in->sin_port = htons( 514 );
 
