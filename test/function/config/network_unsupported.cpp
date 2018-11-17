d3fc3888e3c8445246dbcfe7ecc7e5df6b8bc7f6
Joel Anderson <joelanderson333@gmail.com>
2018-11-17 09:03:49 -0500

    add network unsupported test

---
+++ test/function/config/network_unsupported.cpp
@@ -0,0 +1,49 @@
+// SPDX-License-Identifier: Apache-2.0
+
+/*
+ * Copyright 2018 Joel E. Anderson
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <gtest/gtest.h>
+#include <stddef.h>
+#include <stumpless.h>
+
+namespace {
+
+  TEST( NetworkTargetTest, Unsupported ) {
+    struct stumpless_target target;
+    struct stumpless_entry *entry;
+    struct stumpless_error *error;
+    int result;
+
+    entry = stumpless_new_entry( STUMPLESS_FACILITY_USER,
+                                 STUMPLESS_SEVERITY_INFO,
+                                "stumpless-unit-test",
+                                "basic-entry",
+                                "basic test message" );
+
+    target.type = STUMPLESS_NETWORK_TARGET;
+    target.id = &target;
+
+    result = stumpless_add_entry( &target, entry );
+    EXPECT_LT( result, 0 );
+
+    error = stumpless_get_error(  );
+    ASSERT_TRUE( error != NULL );
+    ASSERT_EQ( error->id, STUMPLESS_TARGET_UNSUPPORTED );
+
+    stumpless_destroy_entry( entry );
+  }
+}
