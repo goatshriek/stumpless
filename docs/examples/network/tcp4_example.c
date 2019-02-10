9b46adebd7688b1777136530f66fc6a381e573ba
Joel Anderson <joelanderson333@gmail.com>
2019-02-10 18:04:42 -0500

    add tcp4 example

--- docs/examples/network/tcp4_example.c
+++ docs/examples/network/tcp4_example.c
@@ -22,9 +22,11 @@
 
 int
 main( int argc, char **argv ) {
-  const char *log_server = "example.com"; // change this if you want to test
+  const char *log_server = "192.168.1.9"; // change this if you want to test
                                           // with your own server
-  struct stumpless_target *udp4_target;
+  const char *port = "5514"; // change this if your server is listening on a
+                             // port other than 5514
+  struct stumpless_target *tcp4_target;
   struct stumpless_target *target_result;
   struct stumpless_entry *basic_entry;
   struct stumpless_element *element;
@@ -39,9 +41,7 @@ main( int argc, char **argv ) {
                                      STUMPLESS_SEVERITY_INFO,
                                      "example-app-name",
                                      "example-msgid",
-                                     "This is an example message, but it is"
-                                     " pretty long. It'll be truncated if the"
-                                     " maximum message size is set too low." );
+                                     "This is an example message." );
   if( !basic_entry ) {
     printf( "could not create a basic entry\n" );
     return EXIT_FAILURE;
@@ -63,39 +63,50 @@ main( int argc, char **argv ) {
   }
 
 
-  // opening the target is straightforward
-  udp4_target = stumpless_open_udp4_target( "udp4-example",
-                                            log_server,
-                                            STUMPLESS_OPTION_NONE,
-                                            STUMPLESS_FACILITY_USER );
-  if( !udp4_target ) {
-    printf( "couldn't open up the udp4 target\n" );
+  // to use the builder style, first we create a new target
+  tcp4_target = stumpless_new_tcp4_target( "tcp4-example" );
+  if( !tcp4_target ) {
+    printf( "couldn't create a new tcp4 target\n" );
+    return EXIT_FAILURE;
+  }
+
+
+  // next we set the destination
+  target_result = stumpless_set_destination( tcp4_target, log_server );
+  if( !target_result ) {
+    printf( "couldn't set the destination for the target\n" );
+    return EXIT_FAILURE;
+  }
+
+
+  // and the transport port
+  target_result = stumpless_set_transport_port( tcp4_target, port );
+  if( !target_result ) {
+    printf( "couldn't set the port for the target\n" );
+    return EXIT_FAILURE;
+  }
+
+
+  // and finally the default facility
+  target_result = stumpless_set_default_facility( tcp4_target,
+                                                  STUMPLESS_FACILITY_LOCAL0 );
+  if( !target_result ) {
+    printf( "couldn't set the default facility of the target\n" );
+    return EXIT_FAILURE;
+  }
+
+
+  // after we've set all of the options, we open the target
+  // this won't work if the server isn't listening and responding
+  target_result = stumpless_open_target( tcp4_target );
+  if( !target_result ) {
+    printf( "couldn't open the target. are you sure that the server is listening on the right port?\n" );
     return EXIT_FAILURE;
   }
 
 
   // sending the entry is just like normal
-  log_result = stumpless_add_entry( udp4_target, basic_entry );
-  if( log_result < 0 ) {
-    printf( "could not log an entry\n" );
-    return EXIT_FAILURE;
-  }
-
-
-  // UDP messages will be truncated if they go over the maximum message size.
-  // This is set to 1472 by default, assuming an MTU of 1500 and a typical
-  // overhead of 28 bytes. However, you can tweak this setting if you need to.
-  // Messages that go over are simply truncated and sent.
-  target_result = stumpless_set_udp_max_message_size( udp4_target, 200 );
-  if( !target_result ) {
-    printf( "could not set the max message size of the target\n" );
-    return EXIT_FAILURE;
-  }
-
-
-  // this entry will be truncated to 200 bytes, meaning that the message will
-  // be cut off partways through
-  log_result = stumpless_add_entry( udp4_target, basic_entry );
+  log_result = stumpless_add_entry( tcp4_target, basic_entry );
   if( log_result < 0 ) {
     printf( "could not log an entry\n" );
     return EXIT_FAILURE;
@@ -103,7 +114,7 @@ main( int argc, char **argv ) {
 
 
   // closing the target is done as usual
-  stumpless_close_network_target( udp4_target );
+  stumpless_close_network_target( tcp4_target );
 
 
   // destroying all the other resources before finishing up:
