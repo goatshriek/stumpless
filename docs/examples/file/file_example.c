9aa68fc983ca36411265514600fa7ccb9bbd2f48
Joel <joelanderson333@gmail.com>
2019-02-20 18:05:21 -0500

    add docs and examples for file targets (#46)
    File targets were missing documentation of their open and close functions, as
    well as a simple example. This change adds this missing documentation.

---
+++ docs/examples/file/file_example.c
@@ -0,0 +1,90 @@
+// SPDX-License-Identifier: Apache-2.0
+
+/*
+ * Copyright 2019 Joel E. Anderson
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <stdlib.h>
+#include <stumpless.h>
+
+int
+main( int argc, char **argv ) {
+  const char *filename = "example.log"; // change this if you want to log to
+                                        // some other file
+  struct stumpless_entry *entry;
+  struct stumpless_element *element;
+  struct stumpless_entry *entry_result;
+  struct stumpless_param *param;
+  struct stumpless_element *element_result;
+  struct stumpless_target *file_target;
+  int log_result;
+
+
+  // building the entry is done as usual
+  entry = stumpless_new_entry( STUMPLESS_FACILITY_USER,
+                                     STUMPLESS_SEVERITY_INFO,
+                                     "example-app-name",
+                                     "example-msgid",
+                                     "This is an example message." );
+  if( !entry ) {
+    stumpless_perror( "could not create a basic entry" );
+    return EXIT_FAILURE;
+  }
+
+  element = stumpless_new_element( "basic-element" );
+  entry_result = stumpless_add_element( entry, element );
+  if( !entry_result ) {
+    stumpless_perror( "could not create and add an element to the entry" );
+    return EXIT_FAILURE;
+  }
+
+
+  param = stumpless_new_param( "basic-param-name", "basic-param-value" );
+  element_result = stumpless_add_param( element, param );
+  if( !element_result ) {
+    stumpless_perror( "could not create and add a param to the element" );
+    return EXIT_FAILURE;
+  }
+
+
+  // the name of a file target is the file that it will log to
+  file_target = stumpless_open_file_target( filename,
+                                            STUMPLESS_OPTION_NONE,
+                                            STUMPLESS_FACILITY_USER );
+  if( !file_target ) {
+    stumpless_perror( "couldn't create a new file target" );
+    return EXIT_FAILURE;
+  }
+
+
+  // sending the entry is just like normal
+  log_result = stumpless_add_entry( file_target, entry );
+  if( log_result < 0 ) {
+    stumpless_perror( "could not log an entry" );
+    return EXIT_FAILURE;
+  }
+
+
+  // closing the target is done as usual
+  stumpless_close_file_target( file_target );
+
+
+  // destroying all the other resources before finishing up:
+  stumpless_destroy_entry( entry );
+  stumpless_free_all(  );
+
+
+  return EXIT_SUCCESS;
+}
--- include/stumpless/target/file.h
+++ include/stumpless/target/file.h
@@ -1,7 +1,7 @@
 /* SPDX-License-Identifier: Apache-2.0 */
 
 /*
- * Copyright 2018 Joel E. Anderson
+ * Copyright 2018-2019 Joel E. Anderson
  * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,6 +16,11 @@
  * limitations under the License.
  */
 
+/** @file
+ * File targets allow logs to be sent to a specified file. Files are created
+ * as needed, and logs are appended to any existing contents.
+ */
+
 #ifndef __STUMPLESS_TARGET_FILE_H
 #  define __STUMPLESS_TARGET_FILE_H
 
@@ -25,13 +30,41 @@
 extern "C" {
 #  endif
 
-  void
-  stumpless_close_file_target( struct stumpless_target *target );
+/**
+ * Closes a file target.
+ *
+ * This function closes the file target, as well as the stream opened to the
+ * file.
+ *
+ * @param target The file target to close.
+ */
+void
+stumpless_close_file_target( struct stumpless_target *target );
 
-  struct stumpless_target *
-  stumpless_open_file_target( const char *name,
-                              int options,
-                              int default_facility );
+/**
+ * Opens a file target.
+ *
+ * File targets send logs to a file on the system. Note that this will open the
+ * specified file, and it will remain open while the file target is open. If you
+ * need to control the file stream yourself, then you should consider using a
+ * stream target instead.
+ *
+ * @param name The name of the logging target, as well as the name of the file
+ * to open.
+ *
+ * @param options The options to use for the logging target. This is a bit-wise
+ * or of one or more \c STUMPLESS_OPTION values.
+ *
+ * @param default_facility The facility code to use for entries that do not have
+ * a facility value set on them. This should be a \c STUMPLESS_FACILITY value.
+ *
+ * @return The opened target if no error is encountered. In the event of an
+ * error, NULL is returned and an error code is set appropriately.
+ */
+struct stumpless_target *
+stumpless_open_file_target( const char *name,
+                            int options,
+                            int default_facility );
 
 #  ifdef __cplusplus
 }                               /* extern "C" */
