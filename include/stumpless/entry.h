e0bcb0a57f1f7f0a030738f0216053bd4eaa8bd4
Joel Anderson <joelanderson333@gmail.com>
2019-03-17 16:26:13 -0400

    add facility and severity documentation

--- include/stumpless/entry.h
+++ include/stumpless/entry.h
@@ -43,39 +43,73 @@ extern "C" {
 #  ifdef STUMPLESS_SYSLOG_H_COMPATIBLE
 #    include <syslog.h>
 
+
 /* severity codes as set by syslog.h */
+
+/** Emergency: system is unusable. */
 #    define STUMPLESS_SEVERITY_EMERG  LOG_EMERG
+/** Alert: action must be taken immediately. */
 #    define STUMPLESS_SEVERITY_ALERT  LOG_ALERT
+/** Critical: critical conditions. */
 #    define STUMPLESS_SEVERITY_CRIT   LOG_CRIT
+/** Error: error conditions. */
 #    define STUMPLESS_SEVERITY_ERR    LOG_ERR
+/** Warning: warning conditions. */
 #    define STUMPLESS_SEVERITY_WARN   LOG_WARN
+/** Notice: normal but significant condition. */
 #    define STUMPLESS_SEVERITY_NOTICE LOG_NOTICE
+/** Informational: informational messages. */
 #    define STUMPLESS_SEVERITY_INFO   LOG_INFO
+/** Debug: debug-level messages. */
 #    define STUMPLESS_SEVERITY_DEBUG  LOG_DEBUG
 
+/** Creates a severity mask for the provided severity. */
 #    define STUMPLESS_SEVERITY_MASK(severity) (LOG_MASK(severity))
+/** Creates a severity mask from EMERG up to the provided severity. */
 #    define STUMPLESS_SEVERITY_MASK_UPTO(severity) (LOG_UPTO(severity))
 
+
 /* facility codes as set by syslog.h */
+
+/** Kernel messages. */
 #    define STUMPLESS_FACILITY_KERN   LOG_KERN
+/** User-level messages. */
 #    define STUMPLESS_FACILITY_USER   LOG_USER
+/** Mail system. */
 #    define STUMPLESS_FACILITY_MAIL   LOG_MAIL
+/** Network news subsystem. */
 #    define STUMPLESS_FACILITY_NEWS   LOG_NEWS
+/** UUCP subsystem. */
 #    define STUMPLESS_FACILITY_UUCP   LOG_UUCP
+/** System daemons. */
 #    define STUMPLESS_FACILITY_DAEMON LOG_DAEMON
+/** Security/authorization messages. */
 #    define STUMPLESS_FACILITY_AUTH   LOG_AUTH
+/** Clock daemon. */
 #    define STUMPLESS_FACILITY_CRON   LOG_CRON
+/** Line printer subsystem. */
 #    define STUMPLESS_FACILITY_LPR    LOG_LPR
+/** Local use 0. */
 #    define STUMPLESS_FACILITY_LOCAL0 LOG_LOCAL0
+/** Local use 1. */
 #    define STUMPLESS_FACILITY_LOCAL1 LOG_LOCAL1
+/** Local use 2. */
 #    define STUMPLESS_FACILITY_LOCAL2 LOG_LOCAL2
+/** Local use 3. */
 #    define STUMPLESS_FACILITY_LOCAL3 LOG_LOCAL3
+/** Local use 4. */
 #    define STUMPLESS_FACILITY_LOCAL4 LOG_LOCAL4
+/** Local use 5. */
 #    define STUMPLESS_FACILITY_LOCAL5 LOG_LOCAL5
+/** Local use 6. */
 #    define STUMPLESS_FACILITY_LOCAL6 LOG_LOCAL6
+/** Local use 7. */
 #    define STUMPLESS_FACILITY_LOCAL7 LOG_LOCAL7
 
+
 /* options defined in syslog.h */
+/* these don't have doxygen documentation as they are currently not supported
+ * by the implementation */
 #    define STUMPLESS_OPTION_PID    LOG_PID
 #    define STUMPLESS_OPTION_CONS   LOG_CONS
 #    define STUMPLESS_OPTION_NDELAY LOG_NDELAY
@@ -126,15 +160,23 @@ extern "C" {
 #  endif
 
 /* remaining facility codes specified in RFC 5424 */
+
+/** Messages generated internally by logging daemon. */
 #  define STUMPLESS_FACILITY_SYSLOG (5<<3)
+/** Security/authorization messages. */
 #  define STUMPLESS_FACILITY_AUTH2  (10<<3)
+/** FTP daemon. */
 #  define STUMPLESS_FACILITY_FTP    (11<<3)
+/** NTP subsystem. */
 #  define STUMPLESS_FACILITY_NTP    (12<<3)
+/** Log audit. */
 #  define STUMPLESS_FACILITY_AUDIT  (13<<3)
+/** Log alert. */
 #  define STUMPLESS_FACILITY_ALERT  (14<<3)
+/** Clock daemon. */
 #  define STUMPLESS_FACILITY_CRON2  (15<<3)
 
-/* custom option code */
+/** Empty option mask for explicit 'no option' use. */
 #  define STUMPLESS_OPTION_NONE 0
 
 /**
