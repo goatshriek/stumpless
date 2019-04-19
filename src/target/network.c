e8d474a5dc46259b68a5db115c422ca62b6746a0
Joel Anderson <joelanderson333@gmail.com>
2019-04-18 21:28:16 -0400

    add unnecessary includes and populate c and c++ manifests

--- src/target/network.c
+++ src/target/network.c
@@ -19,6 +19,7 @@
 #include "private/config/wrapper.h"
 
 #include <stddef.h>
+#include <stdio.h>
 #include <string.h>
 #include <stumpless/target.h>
 #include <stumpless/target/network.h>
--- tools/check_headers/check_headers.rb
+++ tools/check_headers/check_headers.rb
@@ -1,42 +1,42 @@
 #!/usr/bin/env ruby
 
 # as it is, this script makes several assumptions about the structure of the
-# source code that it is analyzing, as well as where certain files are:
+# source code that it is analyzing, where certain files are, and more:
 #  - ruby 2.0 is required due to use of the __dir__ variable
 #  - any line containing '/*' starts a comment
 #  - any line containing '*/' ends a comment
+#  - the manifest files are in the same directory as this script
+#  - for terms that may exist in more than one header, only one of those headers
+#    will be included
 
 require 'yaml'
 
 known_terms = Hash.new
 
-default_manifests = ["c_standard_library.yml", "stumpless.yml"]
+default_manifests = ["c_standard_library.yml", "cpp_standard_library.yml", "stumpless.yml"]
 default_manifests.each do |filename|
   file_path = File.join(__dir__, filename)
   manifest_terms = YAML.load_file file_path
   known_terms.merge! manifest_terms
 end
 
-included_files = Array.new
-used_terms = Array.new
-skipping = false
 ARGV.each do |source_filename|
+  included_files = Array.new
+  used_includes = Array.new
+  used_terms = Array.new
+  skipping = false
+
   File.open(source_filename).each do |line|
+    if line.include? '/*'
+      skipping = true
+    end
+
     if line.include? '*/'
       skipping = false
       next
     end
 
     if skipping
-      next
-    end
-
-    if line.include? '/*'
-      skipping = true
-      next
-    end
-
-    if line.strip.empty?
       next
     end
 
@@ -50,25 +50,30 @@ ARGV.each do |source_filename|
       end
     end
   end
-end
 
-included_files.uniq!
-
-used_terms.uniq.each do |term|
-  requirement = known_terms[term]
-
-  if requirement.is_a? Array
-    met_requirements = requirement & included_files
-    if met_requirements.empty?
-      puts "use of '#{term}' requires inclusion of at least one of #{requirement.join(', ')}"
-    end
-  else
-    unless included_files.include? requirement
-      puts "use of '#{term}' requires inclusion of '#{requirement}'"
+  included_files.uniq!
+ 
+  used_terms.uniq.each do |term|
+    requirement = known_terms[term]
+  
+    if requirement.is_a? Array
+      met_requirements = requirement & included_files
+      if met_requirements.empty?
+        puts "#{source_filename}: use of '#{term}' requires inclusion of at least one of #{requirement.join(', ')}"
+      else
+        used_includes.concat met_requirements
+      end
+    else
+      unless included_files.include? requirement
+        puts "#{source_filename}: use of '#{term}' requires inclusion of '#{requirement}'"
+      else
+        used_includes << requirement
+      end
     end
   end
 
-  if requirement.is_a? Array
+  unused_includes = included_files - used_includes
+  unused_includes.each do |include_file|
+    puts "#{source_filename}: unused include #{include_file}"
   end
-
 end
