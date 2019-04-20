01f33f0a1dfa6e8610ee1669f949ea720b36bcc4
Joel Anderson <joelanderson333@gmail.com>
2019-04-20 09:46:15 -0400

    add support for file globs

--- tools/check_headers/check_headers.rb
+++ tools/check_headers/check_headers.rb
@@ -20,60 +20,70 @@ default_manifests.each do |filename|
   known_terms.merge! manifest_terms
 end
 
-ARGV.each do |source_filename|
+return_code = 0
+
+ARGV.each do |source_glob|
   included_files = Array.new
   used_includes = Array.new
   used_terms = Array.new
   skipping = false
 
-  File.open(source_filename).each do |line|
-    if line.include? '/*'
-      skipping = true
-    end
-
-    if line.include? '*/'
-      skipping = false
-      next
-    end
-
-    if skipping
-      next
-    end
-
-    line.match(/#\s*include\s*["<](.*)[">]/) do |include_match|
-      included_files << include_match[1]
-    end
-
-    line.split(/\W/).each do |word|
-      if known_terms.has_key?(word)
-        used_terms << word
+  Dir.glob(source_glob) do |source_filename|
+    File.open(source_filename).each do |line|
+      # we don't immediately start skipping lines in case this is a one-line comment
+      if line.include? '/*'
+        skipping = true
       end
-    end
-  end
-
-  included_files.uniq!
- 
-  used_terms.uniq.each do |term|
-    requirement = known_terms[term]
   
-    if requirement.is_a? Array
-      met_requirements = requirement & included_files
-      if met_requirements.empty?
-        puts "#{source_filename}: use of '#{term}' requires inclusion of at least one of #{requirement.join(', ')}"
-      else
-        used_includes.concat met_requirements
+      if line.include? '*/'
+        skipping = false
+        next
       end
-    else
-      unless included_files.include? requirement
-        puts "#{source_filename}: use of '#{term}' requires inclusion of '#{requirement}'"
-      else
-        used_includes << requirement
+  
+      if skipping
+        next
+      end
+  
+      line.match(/#\s*include\s*["<](.*)[">]/) do |include_match|
+        included_files << include_match[1]
+      end
+  
+      line.split(/\W/).each do |word|
+        if known_terms.has_key?(word)
+          used_terms << word
+        end
       end
     end
-  end
-
-  unused_includes = included_files - used_includes
-  unused_includes.each do |include_file|
-    puts "#{source_filename}: unused include #{include_file}"
+  
+    included_files.uniq!
+   
+    used_terms.uniq.each do |term|
+      requirement = known_terms[term]
+    
+      if requirement.is_a? Array
+        met_requirements = requirement & included_files
+        if met_requirements.empty?
+          puts "#{source_filename}: use of '#{term}' requires inclusion of at least one of #{requirement.join(', ')}"
+          return_code = 1
+        else
+          used_includes.concat met_requirements
+        end
+      else
+        unless included_files.include? requirement
+          puts "#{source_filename}: use of '#{term}' requires inclusion of '#{requirement}'"
+          return_code = 1
+        else
+          used_includes << requirement
+        end
+      end
+    end
+  
+    unused_includes = included_files - used_includes
+    unused_includes.each do |include_file|
+      puts "#{source_filename}: unused include #{include_file}"
+      return_code = 1
+    end
   end
 end
+
+exit return_code
