c70b3ff4060413b9a8bb88871fad5fd88d10928b
Joel Anderson <joelanderson333@gmail.com>
2019-04-20 10:15:10 -0400

    add copyright to script

--- tools/check_headers/check_headers.rb
+++ tools/check_headers/check_headers.rb
@@ -1,5 +1,20 @@
 #!/usr/bin/env ruby
 
+# Copyright 2019 Joel E. Anderson
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
 # as it is, this script makes several assumptions about the structure of the
 # source code that it is analyzing, where certain files are, and more:
 #  - ruby 2.0 is required due to use of the __dir__ variable
