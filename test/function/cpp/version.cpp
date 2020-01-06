d190208252ebbb6910ebeb9c1dc06c564b4566ed
Joel Anderson <joelanderson333@gmail.com>
2020-01-06 18:13:05 -0500

    add error cmake

--- test/function/cpp/version.cpp
+++ test/function/cpp/version.cpp
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: Apache-2.0
 
 /*
- * Copyright 2019 Joel E. Anderson
+ * Copyright 2020 Joel E. Anderson
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
