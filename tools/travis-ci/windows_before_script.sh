cc3b31a8352b5d7a34adbf17bf2cee8e1377457e
Joel Anderson <joelanderson333@gmail.com>
2020-02-13 18:58:07 -0500

    change compiler path

--- tools/travis-ci/windows_before_script.sh
+++ tools/travis-ci/windows_before_script.sh
@@ -1,3 +1,1 @@
-echo `which gcc`
-echo `which g++`
-cmake -G "Unix Makefiles" -DCMAKE_C_COMPILER="gcc" -DCMAKE_CXX_COMPILER="g++" -DCOVERAGE=ON ${BUILD_ARGS} ..
+cmake -G "Unix Makefiles" -DCMAKE_C_COMPILER="C:/tools/msys64/mingw64/bin/gcc.exe" -DCMAKE_CXX_COMPILER="C:/tools/msys64/mingw64/bin/c++.exe" -DCOVERAGE=ON ${BUILD_ARGS} ..
