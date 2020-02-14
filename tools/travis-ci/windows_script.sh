ad9c491b5ac22ea7950407665f93c048b23b9f34
Joel Anderson <joelanderson333@gmail.com>
2020-02-14 17:46:50 -0500

    add make check into windows

--- tools/travis-ci/windows_script.sh
+++ tools/travis-ci/windows_script.sh
@@ -1,7 +1,3 @@
-ls /c/ProgramData/chocolatey/bin
-ls C:/ProgramData/chocolatey/bin
-mingw32-make all VERBOSE=1 2>&1
-ls
-#make CTEST_OUTPUT_ON_FAILURE=1 VERBOSE=1 check 2>&1
-ls
-mingw32-make examples 2>&1
+mingw32-make all
+mingw32-make CTEST_OUTPUT_ON_FAILURE=1 check
+mingw32-make examples
