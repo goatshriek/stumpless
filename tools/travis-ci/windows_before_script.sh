719c63c32de131bca605be71de0eaba046f1c638
Joel Anderson <joelanderson333@gmail.com>
2020-02-13 19:01:47 -0500

    adjust again

--- tools/travis-ci/windows_before_script.sh
+++ tools/travis-ci/windows_before_script.sh
@@ -1,1 +1,1 @@
-cmake -G "Unix Makefiles" -DCMAKE_C_COMPILER="C:/tools/msys64/mingw64/bin/gcc.exe" -DCMAKE_CXX_COMPILER="C:/tools/msys64/mingw64/bin/c++.exe" -DCOVERAGE=ON ${BUILD_ARGS} ..
+cmake -G "Unix Makefiles" -DCMAKE_C_COMPILER="/tools/msys64/mingw64/bin/gcc.exe" -DCMAKE_CXX_COMPILER="/tools/msys64/mingw64/bin/c++.exe" -DCOVERAGE=ON ${BUILD_ARGS} ..
