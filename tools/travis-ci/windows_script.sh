16d213b83bbce76ed695cf2ab3dc963ff4ec89ad
Joel <joelanderson333@gmail.com>
2020-02-14 05:58:27 -0500

    use mingw32-make

--- tools/travis-ci/windows_script.sh
+++ tools/travis-ci/windows_script.sh
@@ -1,7 +1,7 @@
 ls /c/ProgramData/chocolatey/bin
 ls C:/ProgramData/chocolatey/bin
-make all VERBOSE=1 2>&1
+mingw32-make all VERBOSE=1 2>&1
 ls
 #make CTEST_OUTPUT_ON_FAILURE=1 VERBOSE=1 check 2>&1
 ls
-make examples 2>&1
+mingw32-make examples 2>&1
