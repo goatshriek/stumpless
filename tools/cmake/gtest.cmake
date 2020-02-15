c8fd4dfa642be2d0e57a0638a7d2eb460fa36bda
Joel Anderson <joelanderson333@gmail.com>
2020-02-15 11:26:46 -0500

    fix gtest

--- tools/cmake/gtest.cmake
+++ tools/cmake/gtest.cmake
@@ -1,6 +1,31 @@
 set(local_gtest_binary_dir "${CMAKE_CURRENT_BINARY_DIR}/gtest/src/gtest-build")
-set(local_gtest_shared_dir "${local_gtest_binary_dir}/bin/${CMAKE_CFG_INTDIR}")
+
 set(local_gtest_static_dir "${local_gtest_binary_dir}/lib/${CMAKE_CFG_INTDIR}")
+
+if(CYGWIN OR MINGW OR WIN32)
+  set(local_gtest_shared_dir "${local_gtest_binary_dir}/bin/${CMAKE_CFG_INTDIR}")
+  set(local_gtest_shared_suffix "dll")
+else()
+  set(local_gtest_shared_dir "${local_gtest_static_dir}")
+  set(local_gtest_shared_prefix "lib")
+  set(local_gtest_shared_suffix "so")
+  set(local_gtest_static_prefix "lib")
+  set(local_gtest_static_prefix "a")
+endif()
+
+if(CYGWIN)
+  set(local_gtest_shared_prefix "cyg")
+  set(local_gtest_static_prefix "lib")
+  set(local_gtest_static_suffix "dll.a")
+elseif(MINGW)
+  set(local_gtest_shared_prefix "lib")
+  set(local_gtest_static_prefix "lib")
+  set(local_gtest_static_suffix "dll.a")
+elseif(WIN32)
+  set(local_gtest_shared_prefix "")
+  set(local_gtest_static_prefix "")
+  set(local_gtest_static_suffix "lib")
+endif()
 
 if(CMAKE_BUILD_TYPE STREQUAL "Debug")
   set(local_gtest_debug_suffix "d")
@@ -8,35 +33,13 @@ else()
   set(local_gtest_debug_suffix "")
 endif()
 
-if(CYGWIN)
-  set(local_gtest_shared_prefix "cyg")
-  set(local_gtest_shared_suffix "dll")
-  set(local_gtest_static_prefix "lib")
-  set(local_gtest_static_suffix "dll.a")
-elseif(MINGW)
-  set(local_gtest_shared_prefix "lib")
-  set(local_gtest_shared_suffix "dll")
-  set(local_gtest_static_prefix "lib")
-  set(local_gtest_static_suffix "dll.a")
-elseif(WIN32)
-  set(local_gtest_shared_prefix "")
-  set(local_gtest_shared_suffix "dll")
-  set(local_gtest_static_prefix "")
-  set(local_gtest_static_suffix "lib")
-else()
-  set(local_gtest_shared_prefix "lib")
-  set(local_gtest_shared_suffix "so")
-endif()
-
 set(local_gtest_shared "${local_gtest_shared_dir}/${local_gtest_shared_prefix}gtest${local_gtest_debug_suffix}.${local_gtest_shared_suffix}")
 set(local_gtest_main_shared "${local_gtest_shared_dir}/${local_gtest_shared_prefix}gtest_main${local_gtest_debug_suffix}.${local_gtest_shared_suffix}")
 set(local_gmock_shared "${local_gtest_shared_dir}/${local_gtest_shared_prefix}gmock${local_gtest_debug_suffix}.${local_gtest_shared_suffix}")
 
-if(CYGWIN OR MINGW OR WIN32)
-  set(local_gtest_static "${local_gtest_static_dir}/${local_gtest_static_prefix}gtest${local_gtest_debug_suffix}.${local_gtest_static_suffix}")
-  set(local_gtest_main_static "${local_gtest_static_dir}/${local_gtest_static_prefix}gtest_main${local_gtest_debug_suffix}.${local_gtest_static_suffix}")
-  set(local_gmock_static "${local_gtest_static_dir}/${local_gtest_static_prefix}gmock${local_gtest_debug_suffix}.${local_gtest_static_suffix}")
-endif()
+set(local_gtest_static "${local_gtest_static_dir}/${local_gtest_static_prefix}gtest${local_gtest_debug_suffix}.${local_gtest_static_suffix}")
+set(local_gtest_main_static "${local_gtest_static_dir}/${local_gtest_static_prefix}gtest_main${local_gtest_debug_suffix}.${local_gtest_static_suffix}")
+set(local_gmock_static "${local_gtest_static_dir}/${local_gtest_static_prefix}gmock${local_gtest_debug_suffix}.${local_gtest_static_suffix}")
 
 set(local_gtest_byproducts
   ${local_gtest_shared}
@@ -73,20 +76,20 @@ add_library(libgmock SHARED IMPORTED GLOBAL)
 add_dependencies(libgmock gtest)
 
 set_target_properties(libgtest PROPERTIES
-  IMPORTED_LOCATION ${local_gtest_shared}
-  IMPORTED_IMPLIB ${local_gtest_static}
+  IMPORTED_LOCATION "${local_gtest_shared}"
+  IMPORTED_IMPLIB "${local_gtest_static}"
   IMPORTED_LINK_INTERFACE_LIBRARIES "${CMAKE_THREAD_LIBS_INIT}"
 )
 
 set_target_properties(libgtestmain PROPERTIES
-  IMPORTED_LOCATION ${local_gtest_main_shared}
-  IMPORTED_IMPLIB ${local_gtest_main_static}
+  IMPORTED_LOCATION "${local_gtest_main_shared}"
+  IMPORTED_IMPLIB "${local_gtest_main_static}"
   IMPORTED_LINK_INTERFACE_LIBRARIES "${CMAKE_THREAD_LIBS_INIT}"
 )
 
 set_target_properties(libgmock PROPERTIES
-  IMPORTED_LOCATION ${local_gmock_shared}
-  IMPORTED_IMPLIB ${local_gmock_static}
+  IMPORTED_LOCATION "${local_gmock_shared}"
+  IMPORTED_IMPLIB "${local_gmock_static}"
   IMPORTED_LINK_INTERFACE_LIBRARIES "${CMAKE_THREAD_LIBS_INIT}"
 )
 
