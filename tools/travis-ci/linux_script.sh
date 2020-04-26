4f4d018f29634775a23eb357a6bafb88fdbbe969
Joel <joelanderson333@gmail.com>
2020-04-26 06:52:25 -0400

    fix check-cpp run in ci

--- tools/travis-ci/linux_script.sh
+++ tools/travis-ci/linux_script.sh
@@ -3,5 +3,5 @@
 build-wrapper-linux-x86-64 --out-dir ../bw-output make all || make all
 make CTEST_OUTPUT_ON_FAILURE=1 check
 sudo --preserve-env make CTEST_OUTPUT_ON_FAILURE=1 check
-if [[ $BUILD_ARGS =~ "BUILD_CPP=ON" ]]; then make check-cpp; fi
+if [[ $BUILD_ARGS =~ "ENABLE_CPP=ON" ]]; then make check-cpp; fi
 make examples
