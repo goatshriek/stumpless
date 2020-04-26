1fba75662bbedc750727ac940dc9994297df91ae
Joel <joelanderson333@gmail.com>
2020-04-26 15:31:16 -0400

    correct build error detection (#64)
    CI scripts returned the last exit code, which would hide errors in other
    commands, for example invocations of `make check`. Stringing the commands
    together with `&&` corrects this.
    
    This change revealed an issue with builds on mac osx where library names
    used a `dylib` extension instead of `so`, which is corrected here as well.

--- tools/travis-ci/linux_script.sh
+++ tools/travis-ci/linux_script.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
-build-wrapper-linux-x86-64 --out-dir ../bw-output make all || make all
-make CTEST_OUTPUT_ON_FAILURE=1 check
-sudo --preserve-env make CTEST_OUTPUT_ON_FAILURE=1 check
+build-wrapper-linux-x86-64 --out-dir ../bw-output make all || make all &&
+make CTEST_OUTPUT_ON_FAILURE=1 check &&
+sudo --preserve-env make CTEST_OUTPUT_ON_FAILURE=1 check &&
 make examples
