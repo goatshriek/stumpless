20415768943204dad59e800cd02a5cb0608f3755
Joel Anderson <joelanderson333@gmail.com>
2020-04-26 08:01:07 -0400

    make travis fail when make check-cpp fails

--- tools/travis-ci/linux_script.sh
+++ tools/travis-ci/linux_script.sh
@@ -1,7 +1,7 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
-build-wrapper-linux-x86-64 --out-dir ../bw-output make all || make all
-make CTEST_OUTPUT_ON_FAILURE=1 check
-sudo --preserve-env make CTEST_OUTPUT_ON_FAILURE=1 check
-if [[ $BUILD_ARGS =~ "ENABLE_CPP=ON" ]]; then make check-cpp; fi
+build-wrapper-linux-x86-64 --out-dir ../bw-output make all || make all &&
+make CTEST_OUTPUT_ON_FAILURE=1 check &&
+sudo --preserve-env make CTEST_OUTPUT_ON_FAILURE=1 check &&
+if [[ $BUILD_ARGS =~ "ENABLE_CPP=ON" ]]; then make check-cpp; fi &&
 make examples
