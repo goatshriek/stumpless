7b2b326ab1ab46ad0f49fba03ed5913429bddddf
Joel Anderson <joelanderson333@gmail.com>
2020-04-28 12:15:12 -0400

    add windows c++ travis build

--- tools/travis-ci/windows_script.sh
+++ tools/travis-ci/windows_script.sh
@@ -1,5 +1,6 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
 mingw32-make all &&
 mingw32-make CTEST_OUTPUT_ON_FAILURE=1 check &&
+if [[ $BUILD_ARGS =~ "ENABLE_CPP=ON" ]]; then make check-cpp; fi &&
 mingw32-make examples
