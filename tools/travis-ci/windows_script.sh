3c29931f9ab54bf7da2cda438cadd454f8e7664c
Joel Anderson <joelanderson333@gmail.com>
2020-04-28 14:38:08 -0400

    fix make command in windows build

--- tools/travis-ci/windows_script.sh
+++ tools/travis-ci/windows_script.sh
@@ -2,5 +2,5 @@
 
 mingw32-make all &&
 mingw32-make CTEST_OUTPUT_ON_FAILURE=1 check &&
-if [[ $BUILD_ARGS =~ "ENABLE_CPP=ON" ]]; then make check-cpp; fi &&
+if [[ $BUILD_ARGS =~ "ENABLE_CPP=ON" ]]; then mingw32-make check-cpp; fi &&
 mingw32-make examples
