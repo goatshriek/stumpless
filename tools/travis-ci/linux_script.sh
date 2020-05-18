133b4b60968a7c386b22c844bd24ec4df619915a
Joel <joelanderson333@gmail.com>
2020-05-18 15:22:35 -0400

    improve build and ci (#65)
    Several improvements to the build and ci scripts:
    
    The travis builds no longer use the `ubuntu-toolchain-r-test` apt source to
    specify the gcc and g++ versions.
    
    Make commands in the travis scripts now use the flag `-j 2` to enable
    multithreaded builds.
    
    Sonarcloud has been added to all of the travis builds using linux images.
    
    The project version is added to the sonar properties of the project before
    the scanner is run on the project.
    
    GTEST_PATH and BENCHMARK_PATH can be given during configuration to avoid
    rebuilding Google Test and Benchmark libraries during the build stage.
    Specific information on how to use these can be found in the help strings
    for the cmake variables.

--- tools/travis-ci/linux_script.sh
+++ tools/travis-ci/linux_script.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
-build-wrapper-linux-x86-64 --out-dir ../bw-output make all || make all &&
-make CTEST_OUTPUT_ON_FAILURE=1 check &&
-sudo --preserve-env make CTEST_OUTPUT_ON_FAILURE=1 check &&
-make examples
+build-wrapper-linux-x86-64 --out-dir ../bw-output make -j 2 all || make -j 2 all &&
+make CTEST_OUTPUT_ON_FAILURE=1 -j 2 check &&
+sudo --preserve-env make CTEST_OUTPUT_ON_FAILURE=1 -j 2 check &&
+make -j 2 examples
