d29d71739709cec599b83e47c01df3bc29ee626f
Joel Anderson <joelanderson333@gmail.com>
2020-06-28 10:10:18 -0400

    add troubleshooting which statement

--- tools/travis-ci/windows_script.sh
+++ tools/travis-ci/windows_script.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+which mingw32-make &&
 mingw32-make all &&
 mingw32-make CTEST_OUTPUT_ON_FAILURE=1 check &&
 if [[ $BUILD_ARGS =~ "ENABLE_CPP=ON" ]]; then mingw32-make check-cpp; fi &&
