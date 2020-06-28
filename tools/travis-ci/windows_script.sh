fe26c31afd4d300078e00783ef0a80f6312ce962
Joel Anderson <joelanderson333@gmail.com>
2020-06-28 10:47:33 -0400

    Revert "try MSYS makefiles"
    This reverts commit ec958731973e5307ac0264c693a912a166580726.

--- tools/travis-ci/windows_script.sh
+++ tools/travis-ci/windows_script.sh
@@ -2,8 +2,7 @@
 
 ls /c/ProgramData/chocolatey/bin &&
 which gcov &&
-gcov --version &&
-make all &&
-make CTEST_OUTPUT_ON_FAILURE=1 check &&
-if [[ $BUILD_ARGS =~ "ENABLE_CPP=ON" ]]; then make check-cpp; fi &&
-make examples
+mingw32-make all &&
+mingw32-make CTEST_OUTPUT_ON_FAILURE=1 check &&
+if [[ $BUILD_ARGS =~ "ENABLE_CPP=ON" ]]; then mingw32-make check-cpp; fi &&
+mingw32-make examples
