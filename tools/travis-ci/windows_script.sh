6d521ed7307f281321c9f23e62e9f8b65336ba9a
Joel Anderson <joelanderson333@gmail.com>
2020-06-28 12:01:35 -0400

    remove troubleshooting prints

--- tools/travis-ci/windows_script.sh
+++ tools/travis-ci/windows_script.sh
@@ -1,10 +1,5 @@
 #!/usr/bin/env bash
 
-ls /c/ProgramData/chocolatey/bin &&
-which gcov &&
-gcov --version &&
-gcc --version &&
-x86_64-w64-mingw32-gcc.exe --version &&
 mingw32-make all &&
 mingw32-make CTEST_OUTPUT_ON_FAILURE=1 check &&
 if [[ $BUILD_ARGS =~ "ENABLE_CPP=ON" ]]; then mingw32-make check-cpp; fi &&
