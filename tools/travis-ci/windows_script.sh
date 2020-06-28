99666de3f27ac670f4ed7c63dee72537599cb138
Joel Anderson <joelanderson333@gmail.com>
2020-06-28 10:49:10 -0400

    add gcov version

--- tools/travis-ci/windows_script.sh
+++ tools/travis-ci/windows_script.sh
@@ -2,6 +2,7 @@
 
 ls /c/ProgramData/chocolatey/bin &&
 which gcov &&
+gcov --version &&
 mingw32-make all &&
 mingw32-make CTEST_OUTPUT_ON_FAILURE=1 check &&
 if [[ $BUILD_ARGS =~ "ENABLE_CPP=ON" ]]; then mingw32-make check-cpp; fi &&
