653282307566b307c73287937a112f997922452e
pedestrianlove <j.s.li@gapp.nthu.edu.tw>
2020-07-27 18:38:58 +0800

    Add Macro function to handle error stringification.

--- include/stumpless/error.h
+++ include/stumpless/error.h
@@ -31,81 +31,116 @@ extern "C" {
 #  endif
 
 /**
- * An identifier of the types of errors that might be encountered.
+ * An (enum) identifier of the types of errors that might be encountered.
  *
  * Note that the same error may be encountered in different contexts with a
  * different ultimate cause. To completely diagnose a problem, you will need
  * to look at more than just the error id.
  */
-enum stumpless_error_id {
+
 /**
  * A provided network address was not valid.
  *
- * This might be formatting problem, or it might mean that a DNS lookup failed.
- */
-  STUMPLESS_ADDRESS_FAILURE,
+ * This might be formatting problem, or it might mean that a DNS lookup failed.*/
+//	STUMPLESS_ADDRESS_FAILURE,
 /** A required function parameter was NULL or empty. */
-  STUMPLESS_ARGUMENT_EMPTY,
+//	STUMPLESS_ARGUMENT_EMPTY,
 /** A provided argument was too big, for example to fit into a buffer target. */
-  STUMPLESS_ARGUMENT_TOO_BIG,
+//	STUMPLESS_ARGUMENT_TOO_BIG,
 /**
  * A duplicate of an already existing element was detected in an entry.
  *
  * @since release v1.6.0.
  */
-  STUMPLESS_DUPLICATE_ELEMENT,
+//	STUMPLESS_DUPLICATE_ELEMENT,
 /**
  * No element with the specified characteristics was found.
  *
  * @since release v1.6.0.
  */
-  STUMPLESS_ELEMENT_NOT_FOUND,
+//	STUMPLESS_ELEMENT_NOT_FOUND,
 /** Could not open a file. */
-  STUMPLESS_FILE_OPEN_FAILURE,
+//	STUMPLESS_FILE_OPEN_FAILURE,
 /** Could not write to a file. */
-  STUMPLESS_FILE_WRITE_FAILURE,
+//	STUMPLESS_FILE_WRITE_FAILURE,
 /** A provided index was not in a valid range. */
-  STUMPLESS_INDEX_OUT_OF_BOUNDS,
+//	STUMPLESS_INDEX_OUT_OF_BOUNDS,
 /** A provided facility code did not conform to RFC 5424. */
-  STUMPLESS_INVALID_FACILITY,
+//	STUMPLESS_INVALID_FACILITY,
 /** A target or entry ID was not valid. */
-  STUMPLESS_INVALID_ID,
+//	STUMPLESS_INVALID_ID,
 /**
  * A provided severity code did not conform to RFC 5424.
  *
  * @since release v1.6.0.
  */
-  STUMPLESS_INVALID_SEVERITY,
+//	STUMPLESS_INVALID_SEVERITY,
 /** A memory allocation or reallocation failed. */
-  STUMPLESS_MEMORY_ALLOCATION_FAILURE,
+//	STUMPLESS_MEMORY_ALLOCATION_FAILURE,
 /** The given layer 3 protocol is not supported by this implementation. */
-  STUMPLESS_NETWORK_PROTOCOL_UNSUPPORTED,
+//	STUMPLESS_NETWORK_PROTOCOL_UNSUPPORTED,
 /**
  * No param with the specified characteristics was found.
  *
  * @since release v1.6.0.
  */
-  STUMPLESS_PARAM_NOT_FOUND,
+//	STUMPLESS_PARAM_NOT_FOUND,
 /** Could not bind to a socket. */
-  STUMPLESS_SOCKET_BIND_FAILURE,
+//	STUMPLESS_SOCKET_BIND_FAILURE,
 /** Could not connect to a socket. */
-  STUMPLESS_SOCKET_CONNECT_FAILURE,
+//	STUMPLESS_SOCKET_CONNECT_FAILURE,
 /** Could not create a socket. */
-  STUMPLESS_SOCKET_FAILURE,
+//	STUMPLESS_SOCKET_FAILURE,
 /** Could not send to a socket. */
-  STUMPLESS_SOCKET_SEND_FAILURE,
+//	STUMPLESS_SOCKET_SEND_FAILURE,
 /** Could not write to a stream. */
-  STUMPLESS_STREAM_WRITE_FAILURE,
+//	STUMPLESS_STREAM_WRITE_FAILURE,
 /** The requested operation is not valid on the given target type. */
-  STUMPLESS_TARGET_INCOMPATIBLE,
+//	STUMPLESS_TARGET_INCOMPATIBLE,
 /** The target type provided is unsupported by this implementation. */
-  STUMPLESS_TARGET_UNSUPPORTED,
+//	STUMPLESS_TARGET_UNSUPPORTED,
 /** The given layer 4 protocol is not supported by this implementation. */
-  STUMPLESS_TRANSPORT_PROTOCOL_UNSUPPORTED,
+//	STUMPLESS_TRANSPORT_PROTOCOL_UNSUPPORTED,
 /** Could not close the Windows Event Log. */
-  STUMPLESS_WINDOWS_EVENT_LOG_CLOSE_FAILURE,
+//	STUMPLESS_WINDOWS_EVENT_LOG_CLOSE_FAILURE,
 /** Could not open the Windows Event Log. */
-  STUMPLESS_WINDOWS_EVENT_LOG_OPEN_FAILURE
+//	STUMPLESS_WINDOWS_EVENT_LOG_OPEN_FAILURE
+
+#define FOREACH_ERROR(ERROR)	\
+  ERROR(STUMPLESS_ADDRESS_FAILURE)	\
+  ERROR(STUMPLESS_ARGUMENT_EMPTY)	\
+  ERROR(STUMPLESS_ARGUMENT_TOO_BIG)	\
+  ERROR(STUMPLESS_DUPLICATE_ELEMENT)	\
+  ERROR(STUMPLESS_ELEMENT_NOT_FOUND)	\
+  ERROR(STUMPLESS_FILE_OPEN_FAILURE)	\
+  ERROR(STUMPLESS_FILE_WRITE_FAILURE)	\
+  ERROR(STUMPLESS_INDEX_OUT_OF_BOUNDS)	\
+  ERROR(STUMPLESS_INVALID_FACILITY)	\
+  ERROR(STUMPLESS_INVALID_ID)	\
+  ERROR(STUMPLESS_INVALID_SEVERITY)	\
+  ERROR(STUMPLESS_MEMORY_ALLOCATION_FAILURE)	\
+  ERROR(STUMPLESS_NETWORK_PROTOCOL_UNSUPPORTED)	\
+  ERROR(STUMPLESS_PARAM_NOT_FOUND)	\
+  ERROR(STUMPLESS_SOCKET_BIND_FAILURE)	\
+  ERROR(STUMPLESS_SOCKET_CONNECT_FAILURE)	\
+  ERROR(STUMPLESS_SOCKET_FAILURE)	\
+  ERROR(STUMPLESS_SOCKET_SEND_FAILURE)	\
+  ERROR(STUMPLESS_STREAM_WRITE_FAILURE)	\
+  ERROR(STUMPLESS_TARGET_INCOMPATIBLE)	\
+  ERROR(STUMPLESS_TARGET_UNSUPPORTED)	\
+  ERROR(STUMPLESS_TRANSPORT_PROTOCOL_UNSUPPORTED)	\
+  ERROR(STUMPLESS_WINDOWS_EVENT_LOG_CLOSE_FAILURE)	\
+  ERROR(STUMPLESS_WINDOWS_EVENT_LOG_OPEN_FAILURE)	\
+
+#define GENERATE_ENUM(ENUM) ENUM,
+#define GENERATE_STRING(STRING) #STRING
+	  
+enum stumpless_error_id {
+  FOREACH_ERROR(GENERATE_ENUM)
+};
+
+static const char *stumpless_error_enum_to_string[] = {
+  FOREACH_ERROR(GENERATE_STRING)
 };
 
 /** Information describing an error encountered by the library. */
