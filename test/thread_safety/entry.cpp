71d6ca1ef282ac385b86ab28d23623cb64f7a99b
Joel Anderson <joelanderson333@gmail.com>
2020-08-14 15:20:35 -0400

    update test to use c++ threads

--- test/thread_safety/entry.cpp
+++ test/thread_safety/entry.cpp
@@ -18,38 +18,28 @@
 
 #include <cstddef>
 #include <gtest/gtest.h>
-#include <pthread.h>
 #include <sstream>
 #include <stumpless.h>
+#include <thread>
 #include "test/helper/assert.hpp"
 
 namespace {
   const int THREAD_COUNT = 16;
   const int ITERATION_COUNT = 1000;
 
-  void *
-  read_entry( void *entry_pointer ) {
-    struct stumpless_entry *entry;
-    int thread_id;
-
-    entry = ( struct stumpless_entry * ) entry_pointer;
-
+  void
+  read_entry( struct stumpless_entry *entry ) {
     for( int i = 0; i < ITERATION_COUNT; i++ ) {
       stumpless_get_entry_app_name( entry );
       stumpless_get_entry_msgid( entry );
       stumpless_get_entry_message( entry );
     }
-
-    return NULL;
   }
 
-  void *
-  write_entry( void *entry_pointer ) {
-    struct stumpless_entry *entry;
-    int thread_id;
-
-    entry = ( struct stumpless_entry * ) entry_pointer;
-    thread_id = pthread_self(  );
+  void
+  write_entry( struct stumpless_entry *entry ) {
+    std::thread::id thread_id = std::this_thread::get_id(  );
+    struct stumpless_element *element;
 
     for( int i = 0; i < ITERATION_COUNT; i++ ) {
       std::ostringstream app_stream;
@@ -61,16 +51,20 @@ namespace {
       stumpless_set_entry_msgid( entry, msgid_stream.str(  ).c_str(  ) );
 
       stumpless_set_entry_message( entry, "message number #%d from thread %d", i, thread_id );
-    }
 
-    return NULL;
+      if( i == ITERATION_COUNT / 2 ) {
+        std::ostringstream element_stream;
+        element_stream << "element-" << thread_id;
+        element = stumpless_new_element( element_stream.str(  ).c_str(  ) );
+      }
+    }
   }
 
   TEST( EntryConsistency, SimultaneousReadsAndWrites ) {
     struct stumpless_entry *entry;
     size_t i;
-    pthread_t reader_threads[THREAD_COUNT];
-    pthread_t writer_threads[THREAD_COUNT];
+    std::thread *reader_threads[THREAD_COUNT];
+    std::thread *writer_threads[THREAD_COUNT];
 
     entry = stumpless_new_entry( STUMPLESS_FACILITY_USER,
                                  STUMPLESS_SEVERITY_INFO,
@@ -81,17 +75,17 @@ namespace {
     ASSERT_NOT_NULL( entry );
 
     for( i = 0; i < THREAD_COUNT; i++ ) {
-      pthread_create( &reader_threads[i], NULL, read_entry, ( void * ) entry );
-      pthread_create( &writer_threads[i], NULL, write_entry, ( void * ) entry );
+      reader_threads[i] = new std::thread( read_entry, entry );
+      writer_threads[i] = new std::thread( write_entry, entry );
     }
 
     for( i = 0; i < THREAD_COUNT; i++ ) {
-      pthread_join( reader_threads[i], NULL );
-      pthread_join( writer_threads[i], NULL );
+      reader_threads[i]->join(  );
+      writer_threads[i]->join(  );
     }
 
     // cleanup after the test
-    stumpless_destroy_entry( entry );
+    stumpless_destroy_entry_and_contents( entry );
 
     stumpless_free_all(  );
   }
