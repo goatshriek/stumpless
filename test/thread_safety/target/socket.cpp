08e4da58d8c9003d215a987b4b359bdde3eca87a
Joel Anderson <joelanderson333@gmail.com>
2020-10-17 17:24:20 -0400

    add consistency checks to socket thread safety test

--- test/thread_safety/target/socket.cpp
+++ test/thread_safety/target/socket.cpp
@@ -18,8 +18,14 @@
 
 #include <cstddef>
 #include <gtest/gtest.h>
+#include <string.h>
 #include <stumpless.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <sys/un.h>
 #include <thread>
+#include <unistd.h>
+#include "test/function/rfc5424.hpp"
 #include "test/helper/assert.hpp"
 #include "test/helper/usage.hpp"
 
@@ -27,14 +33,49 @@ namespace {
   const int THREAD_COUNT = 16;
   const int MESSAGE_COUNT = 100;
 
+  void
+  listen_and_validate( int socket, size_t message_count ){
+    size_t i;
+    char buffer[1024];
+    ssize_t msg_len;
+
+    for( i = 0; i < message_count; i++ ) {
+      msg_len = recv( socket, buffer, 1024, 0 );
+      if( msg_len < 0 ) {
+        buffer[0] = '\0';
+      } else {
+        buffer[msg_len] = '\0';
+      }
+
+      TestRFC5424Compliance( buffer );
+    }
+  }
+
   TEST( WriteConsistency, SimultaneousWrites ) {
+    struct sockaddr_un test_socket_addr;
+    int test_socket;
+    const char *socket_name = "sockettargettest";
+    std::thread listener_thread;
     struct stumpless_target *target;
     size_t i;
     std::thread *threads[THREAD_COUNT];
 
+    // setting up the listening socket
+    test_socket_addr.sun_family = AF_UNIX;
+    memcpy(&test_socket_addr.sun_path, socket_name, strlen(socket_name)+1);
+
+    test_socket = socket(test_socket_addr.sun_family, SOCK_DGRAM, 0);
+
+    bind(test_socket,
+         (struct sockaddr *) &test_socket_addr,
+         sizeof(test_socket_addr));
+    listener_thread = std::thread( listen_and_validate,
+                                   test_socket,
+                                   THREAD_COUNT * MESSAGE_COUNT );
+
     // set up the target to log to
-    target = stumpless_open_socket_target( "basic-socket-target",
-                                           NULL,
+    target = stumpless_open_socket_target( socket_name,
+                                           "thread-safety-test-target-socket",
                                            STUMPLESS_OPTION_NONE,
                                            STUMPLESS_FACILITY_USER );
     EXPECT_NO_ERROR;
@@ -48,10 +89,14 @@ namespace {
       threads[i]->join(  );
       delete threads[i];
     }
+    listener_thread.join(  );
 
     // cleanup after the test
     stumpless_close_socket_target( target );
     EXPECT_NO_ERROR;
+
+    close( test_socket );
+    unlink( socket_name );
 
     stumpless_free_all(  );
   }
