e12312dbfa2a6d9ff8351d136a6028b03e3d7d58
Igor Sivy <igorsivy@gmail.com>
2020-10-20 09:41:02 +0200

    Refactor msgid format checking condition

--- src/validate.c
+++ src/validate.c
@@ -42,7 +42,7 @@ bool validate_msgid_format( const char* msgid ) {
   size_t msgid_length = strlen( msgid );
 
   for (size_t i = 0; i < msgid_length; ++i) {
-    if (!(msgid[i] >= 33 && msgid[i] <= 126)) {
+    if (msgid[i] < 33 || msgid[i] > 126) {
       raise_invalid_encoding(L10N_FORMAT_ERROR_MESSAGE("msgid"));
       return false;
     }
