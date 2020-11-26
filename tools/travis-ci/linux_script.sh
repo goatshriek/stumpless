358f40c1d9840d54da4e75da100729885b3e469f
Joel <joelanderson333@gmail.com>
2020-11-26 10:51:05 -0500

    remove old travis scripts (#141)

--- tools/travis-ci/linux_script.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-build-wrapper-linux-x86-64 --out-dir ../bw-output make -j 2 all || make -j 2 all &&
-make CTEST_OUTPUT_ON_FAILURE=1 -j 2 check &&
-sudo --preserve-env make CTEST_OUTPUT_ON_FAILURE=1 -j 2 check &&
-if [[ $BUILD_ARGS =~ "ENABLE_CPP=ON" ]]; then make -j 2 check-cpp; fi &&
-if [[ $BUILD_ARGS =~ "CMAKE_BUILD_TYPE=Release" ]]; then make -j 2 bench; fi &&
-make -j 2 examples &&
-sudo --preserve-env make install
--- tools/travis-ci/windows_before_script.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env sh
-
-cmake -G "MinGW Makefiles" -DCMAKE_SH="CMAKE_SH-NOTFOUND" -DCOVERAGE=ON ${BUILD_ARGS} ..
