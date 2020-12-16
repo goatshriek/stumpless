adc7ffbacb3f47c5c02307bcd47e8e023f2b877a
Joel Anderson <joelanderson333@gmail.com>
2020-12-16 06:00:00 -0500

    translate one error message

--- include/private/config/locale/it-it.h
+++ include/private/config/locale/it-it.h
@@ -23,7 +23,7 @@
 "could not bind to the local unix socket"
 
 #  define L10N_BUFFER_TOO_SMALL_ERROR_MESSAGE \
-"buffer is too small for the given message"
+"il memoria tampone è troppo piccolo per il messaggio"
 
 #  define L10N_CLOSE_UNSUPPORTED_TARGET_ERROR_MESSAGE \
 "attempted to close an unsupported target type"
