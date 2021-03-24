119d2a2872c1a92bcf688f0962b7a9246c778575
Davide <49058178+Boccolo@users.noreply.github.com>
2021-03-24 16:41:45 +0100

    Fix translations #156

--- include/private/config/locale/it-it.h
+++ include/private/config/locale/it-it.h
@@ -20,135 +20,135 @@
 #  define __STUMPLESS_PRIVATE_CONFIG_LOCALE_IT_IT_H
 
 #  define L10N_BIND_UNIX_SOCKET_FAILED_ERROR_MESSAGE \
-"non ha potuto legare il unix socket locale"
+"bind con socket locale unix fallita"
 
 #  define L10N_BUFFER_TOO_SMALL_ERROR_MESSAGE \
 "il buffer è troppo piccolo per il messaggio"
 
 #  define L10N_CLOSE_UNSUPPORTED_TARGET_ERROR_MESSAGE \
-"ha provato chiudere un tipo di bersaglio non supportato"
+"tentativo di chiusura di un target non supportato"
 
 #  define L10N_CONNECT_SYS_SOCKET_FAILED_ERROR_MESSAGE \
-"connect ha fallito con un socket sys/socket.h"
+"connect fallita con un socket sys/socket.h"
 
 #  define L10N_CONNECT_WIN_SOCKET_FAILED_ERROR_MESSAGE \
-"connect ha fallito con un socket winsock2"
+"connect fallita con un socket winsock2"
 
 #  define L10N_DESTINATION_NETWORK_ONLY_ERROR_MESSAGE \
-"la destinazione è solo valida per bersaglio rete"
+"la destinazione è valida solo per target di rete"
 
 #  define L10N_DUPLICATE_ELEMENT_ERROR_MESSAGE \
-"un elemento con il nome dato è già in questa voce"
+"un elemento con il nome dato è già presente in questa voce"
 
 #  define L10N_ELEMENT_NOT_FOUND_ERROR_MESSAGE \
-"un elemento con le caratteristiche date non ha potuto essere trovato"
+"non è stato possibile trovare un elemento con le caratteristiche date"
 
 #  define L10N_ERRNO_ERROR_CODE_TYPE \
-"errno dopo il fallimento"
+"errno dopo la chiamata fallita"
 
 #  define L10N_LOCAL_SOCKET_NAME_FILE_OPEN_ERROR_MESSAGE \
-"non ha potuto costruire un file con il nome socket dato con mkstemp"
+"non è stato possibile creare un file con il nome scelto per la socket locale usando mkstemp"
 
 #  define L10N_GETADDRINFO_FAILURE_ERROR_MESSAGE \
-"getaddrinfo ha fallito con l'hostname dato"
+"getaddrinfo fallita con l'hostname dato"
 
 #  define L10N_GETADDRINFO_RETURN_ERROR_CODE_TYPE \
-"il codice ritornato del getaddrinfo"
+"il codice di ritorno della chiamata getaddrinfo fallita"
 
 #  define L10N_GETCOMPUTERNAME_FAILED_ERROR_MESSAGE \
-"GetComputerName ha fallito"
+"GetComputerName fallita"
 
 #  define L10N_GETHOSTNAME_FAILED_ERROR_MESSAGE \
-"gethostname ha fallito"
+"gethostname fallita"
 
 #  define L10N_GETLASTERROR_ERROR_CODE_TYPE \
-"il risultato di GetLastError dopo il fallimento"
+"il risultato di GetLastError dopo la chiamata fallita"
 
 #  define L10N_INDEX_OUT_OF_BOUNDS_ERROR_CODE_TYPE \
-"l'indice non valido, oppure -1 se è troppo grande rappresentare come un int"
+"l'indice non valido, oppure -1 se è troppo grande da rappresentare con un int"
 
 #  define L10N_INVALID_FACILITY_ERROR_CODE_TYPE \
 "il servizio non valido"
 
 #  define L10N_INVALID_FACILITY_ERROR_MESSAGE \
-"i codici servizio devono soddisfare RFC 5424, dopo la moltiplicazione con 8"
+"i codici di servizio devono essere definiti in osservanza del RFC 5424, dopo la moltiplicazione per 8"
 
 #  define L10N_INVALID_INDEX_ERROR_MESSAGE( INDEXED_THING ) \
-"non valido " INDEXED_THING " indice"
+"indice " INDEXED_THING " non valido"
 
 #  define L10N_INVALID_SEVERITY_ERROR_CODE_TYPE \
-"la gravità non valida"
+"gravità non valida"
 
 #  define L10N_INVALID_SEVERITY_ERROR_MESSAGE \
-"i codici gravità devono soddisfare RFC 5424: tra 0 e 7, compreso"
+"i codici gravità devono essere definiti in osservanza del RFC 5424: tra 0 e 7, compreso"
 
 #  define L10N_MAX_MESSAGE_SIZE_UDP_ONLY_ERROR_MESSAGE \
-"il massimo dimensione è solo valido per i bersagli reti UDP"
+"la dimensione massima del messaggio è solo valido per target di rete UDP"
 
 #  define L10N_MESSAGE_TOO_BIG_FOR_DATAGRAM_ERROR_MESSAGE \
-"il messaggio è troppo grande per mandare in unico datagram"
+"il messaggio è troppo grande per essere inviato in unico datagram"
 
 #  define L10N_MESSAGE_SIZE_ERROR_CODE_TYPE \
-"la dimensione del messagio"
+"la dimensione del messaggio che si è tentato di inviare"
 
 #  define L10N_NETWORK_TARGETS_UNSUPPORTED \
-"i bersagli reti non sono supportato"
+"i target di rete non sono supportati in questa build"
 
 #  define L10N_NULL_ARG_ERROR_MESSAGE( ARG_NAME ) \
 ARG_NAME " era NULL"
 
 #  define L10N_OPEN_UNSUPPORTED_TARGET_ERROR_MESSAGE \
-"ha provato aprire un tipo di bersaglio non supportato"
+"tentativo di apertura di un target di tipo non supportato"
 
 #  define L10N_PARAM_NOT_FOUND_ERROR_MESSAGE \
-"un parametro con le caratteristiche date non ha potuto essere trovato"
+"non è stato possibile trovare un parametro con le caratteristiche definite"
 
 #  define L10N_SEND_ENTRY_TO_UNSUPPORTED_TARGET_ERROR_MESSAGE \
-"ha provato mandare una voce a un tipo di bersaglio non supportato"
+"tentativo di invio di una voce a un tipo di target non supportato"
 
 #  define L10N_SEND_MESSAGE_TO_UNSUPPORTED_TARGET_ERROR_MESSAGE \
-"ha provato mandare un messaggio a un tipo di bersaglio non supportato"
+"tentativo di invio di un messaggio a un tipo di target non supportato"
 
 #  define L10N_SEND_SYS_SOCKET_FAILED_ERROR_MESSAGE \
-"send ha fallito con un socket sys/socket.h"
+"send fallita con un socket sys/socket.h"
 
 #  define L10N_SENDTO_UNIX_SOCKET_FAILED_ERROR_MESSAGE \
-"sendto ha fallito con un socket unix"
+"sendto fallita con un socket unix"
 
 #  define L10N_SEND_WIN_SOCKET_FAILED_ERROR_MESSAGE \
-"send ha fallito con un socket winsock2"
+"send fallita con un socket winsock2"
 
 #  define L10N_SOCKET_FAILED_ERROR_MESSAGE \
-"ha fallito aprire un socket"
+"fallita apertura di un socket"
 
 #  define L10N_TARGET_ALWAYS_OPEN_ERROR_MESSAGE \
-"questo tipo di bersaglio è sempre aperto"
+"questo tipo di target è sempre aperto"
 
 #  define L10N_TRANSPORT_PORT_NETWORK_ONLY_ERROR_MESSAGE \
-"i porti trasporti sono solo valido per bersagli reti"
+"le porte di trasporto sono solo valide per target di rete"
 
 #  define L10N_UNIX_SOCKET_FAILED_ERROR_MESSAGE \
-"ha fallito aprire un socket unix usando la funzione socket"
+"fallita apertura di un socket unix usando la funzione socket"
 
 #  define L10N_UNSUPPORTED_TARGET_IS_OPEN_ERROR_MESSAGE \
-"ha controllato se un tipo di bersaglio non supportato era aperto"
+"controllato se un tipo di target non supportato era aperto"
 
 #  define L10N_WINDOWS_SOCKET_ERROR_CODE_TYPE \
-"il codice errore di Windows Socket"
+"codice di errore di Windows Socket"
 
 #  define L10N_WINSOCK2_SOCKET_FAILED_ERROR_MESSAGE \
-"ha fallito aprire un socket winsock2"
+"fallita apertura di un socket winsock2"
 
 #  define L10N_WSAGETLASTERROR_ERROR_CODE_TYPE \
-"il risultato di WSAGetLastError dopo il fallimento"
+"il risultato di WSAGetLastError dopo la chiamata fallita"
 
 #  define L10N_STRING_TOO_LONG_ERROR_MESSAGE \
-"la lunghezza della stringa era troppo lunga"
+"la lunghezza della stringa eccede il limite massimo"
 
 #  define L10N_STRING_LENGTH_ERROR_CODE_TYPE \
 "la lunghezza della stringa non valida"
 
 #  define L10N_FORMAT_ERROR_MESSAGE(ARG) \
-"il formato di " ARG " era non valido"
+"formato di " ARG " non valido"
 
 #endif /* __STUMPLESS_PRIVATE_CONFIG_LOCALE_IT_IT_H */
