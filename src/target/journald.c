cae5336bfe97f727ed224bc442f235e7b8273597
Joel Anderson <joelanderson333@gmail.com>
2021-05-12 19:58:32 -0400

    add source module

---
+++ src/target/journald.c
@@ -0,0 +1,68 @@
+// SPDX-License-Identifier: Apache-2.0
+
+/*
+ * Copyright 2021 Joel E. Anderson
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <stddef.h>
+#include <stumpless/entry.h>
+#include <stumpless/target.h>
+#include <stumpless/target/journald.h>
+#include "private/config/locale/wrapper.h"
+#include "private/error.h"
+#include "private/target.h"
+#include "private/target/journald.h"
+#include "private/validate.h"
+
+void
+stumpless_close_journald_target( const struct stumpless_target *target ) {
+  if( !target ) {
+    raise_argument_empty( L10N_NULL_ARG_ERROR_MESSAGE( "target" ) );
+    return;
+  }
+
+  if( target->type != STUMPLESS_JOURNALD_TARGET ) {
+    raise_target_incompatible( L10N_INVALID_TARGET_TYPE_ERROR_MESSAGE );
+    return;
+  }
+
+  destroy_target( target );
+  clear_error(  );
+}
+
+struct stumpless_target *
+stumpless_open_journald_target( const char *name ) {
+  struct stumpless_target *target;
+
+  VALIDATE_ARG_NOT_NULL( name );
+
+  target = new_target( STUMPLESS_JOURNALD_TARGET, name );
+  if( !target ) {
+    return NULL;
+  }
+
+  target->id = NULL;
+
+  stumpless_set_current_target( target );
+  return target;
+}
+
+/* private definitions */
+
+int
+send_entry_to_journald_target( const struct stumpless_target *target,
+                               const struct stumpless_entry *entry ) {
+  return -1;
+}
