d184c8370bca6fa51359661980c8040d63728ef3
Ulises Martínez Elías <ulises.mtz.el@gmail.com>
2021-07-05 19:02:07 -0500

    implement spanish (es-ES) localization (#185)
    This fixes #120, which can be referenced for more details.

--- include/private/config/locale/es-es.h
+++ include/private/config/locale/es-es.h
@@ -20,171 +20,173 @@
 #  define __STUMPLESS_PRIVATE_CONFIG_LOCALE_ES_ES_H
 
 #  define L10N_BIND_UNIX_SOCKET_FAILED_ERROR_MESSAGE \
-"L10N BIND UNIX SOCKET FAILED ERROR MESSAGE"
+"no se pudo vincular con el socket unix local"
 
 #  define L10N_BUFFER_TOO_SMALL_ERROR_MESSAGE \
-"L10N BUFFER TOO SMALL ERROR MESSAGE"
+"el buffer es demasiado pequeño para el mensaje proporcionado"
 
 #  define L10N_CLOSE_UNSUPPORTED_TARGET_ERROR_MESSAGE \
-"L10N CLOSE UNSUPPORTED TARGET ERROR MESSAGE"
+"se ha intentado cerrar un tipo de objetvo no soportado"
 
 #  define L10N_CONNECT_SYS_SOCKET_FAILED_ERROR_MESSAGE \
-"L10N CONNECT SYS SOCKET FAILED ERROR MESSAGE"
+"se ha fallado al conectar socket sys/socket.h"
 
 #  define L10N_CONNECT_WIN_SOCKET_FAILED_ERROR_MESSAGE \
-"L10N CONNECT WIN SOCKET FAILED ERROR MESSAGE"
+"se ha fallado al conectar socket winsock2"
 
 #  define L10N_DESTINATION_NETWORK_ONLY_ERROR_MESSAGE \
-"L10N DESTINATION NETWORK ONLY ERROR MESSAGE"
+"el destino es válido sólo para objetivos de red"
 
 #  define L10N_DUPLICATE_ELEMENT_ERROR_MESSAGE \
-"L10N DUPLICATE ELEMENT ERROR MESSAGE"
+"un elemento con este nombre ya está presente en esta entrada"
 
 #  define L10N_ELEMENT_NOT_FOUND_ERROR_MESSAGE \
-"L10N ELEMENT NOT FOUND ERROR MESSAGE"
+"un elemento con las caraterísticas especificadas no pudo ser encontrado"
 
 #  define L10N_ERRNO_ERROR_CODE_TYPE \
-"L10N ERRNO ERROR CODE TYPE"
+"errno despues de la llamada fallida"
 
 #  define L10N_FILE_OPEN_FAILURE_ERROR_MESSAGE \
-"FILE OPEN FAILURE MESSAGE"
+"no se pudo abrir el archivo especificado"
 
 #  define L10N_FILE_WRITE_FAILURE_ERROR_MESSAGE \
-"FILE WRITE FAILURE MESSAGE"
+"no se pudo escribir en el archivo"
 
 #  define L10N_FUNCTION_TARGET_FAILURE_CODE_TYPE \
-"FUNCTION TARGET FAILURE CODE TYPE"
+"código de retorno de la función manager log"
 
 #  define L10N_FUNCTION_TARGET_FAILURE_ERROR_MESSAGE \
-"FUNCTION TARGET FAILURE ERROR MESSAGE"
+"el manejo del log para el objetivo de la función ha fallado"
 
 #  define L10N_LOCAL_SOCKET_NAME_FILE_OPEN_ERROR_MESSAGE \
-"L10N LOCAL SOCKET NAME FILE OPEN ERROR MESSAGE"
+"no se pudo crear un archivo con el nombre de socket local elegido usando mkstemp"
 
 #  define L10N_GETADDRINFO_FAILURE_ERROR_MESSAGE \
-"L10N GETADDRINFO FAILURE ERROR MESSAGE"
+"getaddrinfo ha fallado en el hostname proporcionado"
 
 #  define L10N_GETADDRINFO_RETURN_ERROR_CODE_TYPE \
-"L10N GETADDRINFO RETURN ERROR CODE TYPE"
+"código de retorno de la llamada fallida a getaddrinfo"
 
 #  define L10N_GETCOMPUTERNAME_FAILED_ERROR_MESSAGE \
-"GETCOMPUTERNAME FAILED ERROR MESSAGE"
+"GetComputerName ha fallado"
 
 #  define L10N_GETHOSTNAME_FAILED_ERROR_MESSAGE \
-"GETHOSTNAME FAILED ERROR MESSAGE"
+"gethostname ha fallado"
 
 #  define L10N_GETLASTERROR_ERROR_CODE_TYPE \
-"GETLASTERROR ERROR CODE TYPE"
+"el resultado de GetLastError despues de la llamada fallida"
 
 #  define L10N_INDEX_OUT_OF_BOUNDS_ERROR_CODE_TYPE \
-"L10N INDEX OUT OF BOUNDS ERROR CODE TYPE"
+"el índice no válido, o -1 por lo que es deamasiado largo para representarse como entero"
 
 #  define L10N_INVALID_FACILITY_ERROR_CODE_TYPE \
-"L10N INVALID FACILITY ERROR CODE TYPE"
+"facilidad no válida"
 
 #  define L10N_INVALID_FACILITY_ERROR_MESSAGE \
-"L10N INVALID FACILITY ERROR MESSAGE"
+"Los códigos de facilidad deben ser definidos de acuerdo al RFC 5424" \
+" despues de la multiplicación por 8"
 
 #  define L10N_INVALID_ID_ERROR_MESSAGE \
-"INVALID ID ERROR MESSAGE"
+"id de objetivo no válido"
 
 #  define L10N_INVALID_INDEX_ERROR_MESSAGE( INDEXED_THING ) \
-"L10N INVALID INDEX ERROR MESSAGE( INDEXED THING )"
+"índice " INDEXED_THING " no válido"
 
 #  define L10N_INVALID_SEVERITY_ERROR_CODE_TYPE \
-"L10N INVALID SEVERITY ERROR CODE TYPE"
+"la severidad no válida"
 
 #  define L10N_INVALID_SEVERITY_ERROR_MESSAGE \
-"L10N INVALID SEVERITY ERROR MESSAGE"
+"los códigos de severidad deben ser definidos de acuerdo al RFC 5424:" \
+" valores entre 0 y 7 inclusive"
 
 #  define L10N_MAX_MESSAGE_SIZE_UDP_ONLY_ERROR_MESSAGE \
-"L10N MAX MESSAGE SIZE UDP ONLY ERROR MESSAGE"
+"el tamaño máximo de mensaje sólo es válido para objetivos de red UDP"
 
 #  define L10N_MEMORY_ALLOCATION_FAILURE_ERROR_MESSAGE \
-"MEMORY ALLOCATION FAILURE MESSAGE"
+"el alojamiento de memoria para una llamada ha fallado"
 
 #  define L10N_MESSAGE_TOO_BIG_FOR_DATAGRAM_ERROR_MESSAGE \
-"L10N MESSAGE TOO BIG FOR DATAGRAM ERROR MESSAGE"
+"el mensaje es demasiado largo para ser enviado en un datagrama simple"
 
 #  define L10N_MESSAGE_SIZE_ERROR_CODE_TYPE \
-"L10N MESSAGE SIZE ERROR CODE TYPE"
+"el tamaño del mensaje que se ha intentado enviar"
 
 #  define L10N_NETWORK_PROTOCOL_UNSUPPORTED_ERROR_MESSAGE \
-"NETWORK PROTOCOL UNSUPPORTED ERROR MESSAGE"
+"protocolo de red elegido no soportado"
 
 #  define L10N_NETWORK_TARGETS_UNSUPPORTED \
-"L10N NETWORK TARGETS UNSUPPORTED"
+"objetivos de red no soportados por esta compilación"
 
 #  define L10N_NULL_ARG_ERROR_MESSAGE( ARG_NAME ) \
-"L10N NULL ARG ERROR MESSAGE( ARG NAME )"
+ARG_NAME " fue NULL"
 
 #  define L10N_OPEN_UNSUPPORTED_TARGET_ERROR_MESSAGE \
-"L10N OPEN UNSUPPORTED TARGET ERROR MESSAGE"
+"se ha tratado de abrir un tipo de objetivo no soportado"
 
 #  define L10N_PARAM_NOT_FOUND_ERROR_MESSAGE \
-"L10N PARAM NOT FOUND ERROR MESSAGE"
+"un parámetro con las características especificadas no fue encontrado"
 
 #  define L10N_SEND_ENTRY_TO_UNSUPPORTED_TARGET_ERROR_MESSAGE \
-"L10N SEND ENTRY TO UNSUPPORTED TARGET ERROR MESSAGE"
+"se ha intentado enviar una entrada a un tipo de objetivo no soportado"
 
 #  define L10N_SEND_MESSAGE_TO_UNSUPPORTED_TARGET_ERROR_MESSAGE \
-"L10N SEND MESSAGE TO UNSUPPORTED TARGET ERROR MESSAGE"
+"se ha intentado enviar un mensaje a un tipo de objetivo no soportado"
 
 #  define L10N_SEND_SYS_SOCKET_FAILED_ERROR_MESSAGE \
-"L10N SEND SYS SOCKET FAILED ERROR MESSAGE"
+"se ha fallado al enviar con socket sys/socket.h"
 
 #  define L10N_SENDTO_UNIX_SOCKET_FAILED_ERROR_MESSAGE \
-"L10N SENDTO UNIX SOCKET FAILED ERROR MESSAGE"
+"se ha fallado al enviar con socket unix"
 
 #  define L10N_SEND_WIN_SOCKET_FAILED_ERROR_MESSAGE \
-"L10N SEND WIN SOCKET FAILED ERROR MESSAGE"
+"se ha fallado al enviar con socket winsock2"
 
 #  define L10N_SOCKET_FAILED_ERROR_MESSAGE \
-"L10N SOCKET FAILED ERROR MESSAGE"
+"se ha fallado al abrir el socket"
 
 #  define L10N_STREAM_WRITE_FAILURE_ERROR_MESSAGE \
-"STREAM WRITE FAILURE ERROR MESSAGE"
+"no se pudo escribir en el stream"
 
 #  define L10N_TARGET_ALWAYS_OPEN_ERROR_MESSAGE \
-"L10N TARGET ALWAYS OPEN ERROR MESSAGE"
+"este tipo de objetivo siempre permanece abierto"
 
 #  define L10N_TRANSPORT_PORT_NETWORK_ONLY_ERROR_MESSAGE \
-"L10N TRANSPORT PORT NETWORK ONLY ERROR MESSAGE"
+"los puertos de transporte sólo son válidos para objetivos de red"
 
 #  define L10N_TRANSPORT_PROTOCOL_UNSUPPORTED_ERROR_MESSAGE \
-"TRANSPORT PROTOCOL UNSUPPORTED ERROR MESSAGE"
+"protocolo de transporte elegido no soportado"
 
 #  define L10N_UNIX_SOCKET_FAILED_ERROR_MESSAGE \
-"L10N UNIX SOCKET FAILED ERROR MESSAGE"
+"se ha fallado al abrir un socket unix con la función socket"
 
 #  define L10N_UNSUPPORTED_TARGET_IS_OPEN_ERROR_MESSAGE \
-"L10N UNSUPPORTED TARGET IS OPEN ERROR MESSAGE"
+"se ha revisado si un tipo de objetivo no soportado fue abierto"
 
 #  define L10N_WEL_CLOSE_FAILURE_ERROR_MESSAGE \
-"WEL CLOSE FAILURE ERROR MESSAGE"
+"no se pudo cerrar el log de eventos de Windows"
 
 #  define L10N_WEL_OPEN_FAILURE_ERROR_MESSAGE \
-"WEL OPEN FAILURE ERROR MESSAGE"
+"no se pudo abrir el log de eventos de Windows"
 
 #  define L10N_WINDOWS_SOCKET_ERROR_CODE_TYPE \
-"L10N WINDOWS SOCKET ERROR CODE TYPE"
+"código de error de socket de Windows"
 
 #  define L10N_WINSOCK2_SOCKET_FAILED_ERROR_MESSAGE \
-"L10N WINSOCK2 SOCKET FAILED ERROR MESSAGE"
+"se ha fallado al abrir un socket winsock2"
 
 #  define L10N_WSAGETLASTERROR_ERROR_CODE_TYPE \
-"L10N WSAGETLASTERROR ERROR CODE TYPE"
+"el resultado de WSAGetLastError despues que la llamada fallara"
 
 #  define L10N_STRING_TOO_LONG_ERROR_MESSAGE \
-"STRING TOO LONG"
+"el largo de la cadena ha excedido el límite máximo"
 
 #  define L10N_STRING_LENGTH_ERROR_CODE_TYPE \
-"STRING LENGTH ERROR CODE TYPE"
+"el largo de la cadena infractora"
 
 #  define L10N_FORMAT_ERROR_MESSAGE(ARG) \
-"invalid " ARG " format"
+"formato " ARG " no válido"
 
 #  define L10N_INVALID_TARGET_TYPE_ERROR_MESSAGE \
-"INVALID TARGET TYPE ERROR MESSAGE"
+"MENSAJE DE ERROR POR TIPO DE OBJETIVO NO VÁLIDO"
 
 #endif /* __STUMPLESS_PRIVATE_CONFIG_LOCALE_ES_ES_H */
