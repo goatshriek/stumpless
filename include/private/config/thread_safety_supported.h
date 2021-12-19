c43dd7a6d60786a332b8cbe0e9fd0aa233f9e676
Joel <joelanderson333@gmail.com>
2021-12-19 00:15:21 -0500

    move mutexes to structure definitions (#204)
    In the initial implementation of multithreading support, the mutex used to
    protect target, entry, element, and param types was stored at the end of
    the struct in an effort to avoid a dereference operation and simplify
    initialization.
    
    Unfortunately, this leads to a mismatch in the memory required for a proper
    target structure and the size of the structure according to the compiler.
    This in turn can lead to undefined behavior when using a stack-allocated
    structure. While this isn't currently supported usage, it is planned for
    future releases, and is also used by some test suites (unsupported target
    types being one) which leads to instability.
    
    This change adds a void pointer to the end of the structure instead which
    points at the mutex to use for synchronization of these types. These are
    allocated using a dedicated slab cache instead of as part of the struct's
    initial allocation.
    
    This fixes #203, which can be referenced for more details.

---
+++ include/private/config/thread_safety_supported.h
@@ -0,0 +1,82 @@
+/* SPDX-License-Identifier: Apache-2.0 */
+
+/*
+ * Copyright 2021 Joel E. Anderson
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef __STUMPLESS_PRIVATE_CONFIG_THREAD_SAFETY_SUPPORTED_H
+#  define __STUMPLESS_PRIVATE_CONFIG_THREAD_SAFETY_SUPPORTED_H
+
+#  include "private/config/wrapper/thread_safety.h"
+
+/**
+ * Destroys the mutex and releases its memory.
+ *
+ * **Thread Safety: MT-Unsafe**
+ * This function is not thread safe as it destroys resources that other threads
+ * would use if they tried to use the mutex.
+ *
+ * **Async Signal Safety: AS-Unsafe lock**
+ * This function is not safe to call from signal handlers due to the destruction
+ * of a lock that may be in use.
+ *
+ * **Async Cancel Safety: AC-Unsafe lock**
+ * This function is not safe to call from threads that may be asynchronously
+ * cancelled, as the cleanup of the lock may not be completed.
+ *
+ * @param mutex The mutex to destroy.
+ */
+void
+thread_safety_destroy_mutex( const config_mutex_t *mutex );
+
+/**
+ * Frees all memory used for thread safety support.
+ *
+ * **Thread Safety: MT-Unsafe**
+ * This function is not thread safe as it destroys resources that other threads
+ * might be using.
+ *
+ * **Async Signal Safety: AS-Unsafe heap**
+ * This function is not safe to call from signal handlers due to the use of the
+ * memory deallocation function to release memory.
+ *
+ * **Async Cancel Safety: AC-Unsafe heap**
+ * This function is not safe to call from threads that may be asynchronously
+ * cancelled, as the memory deallocation function may not be AC-Safe itself.
+ */
+void
+thread_safety_free_all( void );
+
+/**
+ * Creates a new mutex and initializes it for usage.
+ *
+ * **Thread Safety: MT-Safe**
+ * This function is thread safe.
+ *
+ * **Async Signal Safety: AS-Unsafe heap**
+ * This function is not safe to call from signal handlers due to the possible
+ * use of memory management functions to create the new mutex.
+ *
+ * **Async Cancel Safety: AC-Unsafe heap**
+ * This function is not safe to call from threads that may be asynchronously
+ * cancelled, due to the possible use of memory management functions.
+ *
+ * @return The created and initialized mutex, or NULL if an error was
+ * encountered.
+ */
+config_mutex_t *
+thread_safety_new_mutex( void );
+
+#endif /* __STUMPLESS_PRIVATE_CONFIG_THREAD_SAFETY_SUPPORTED_H */
--- include/private/target.h
+++ include/private/target.h
@@ -1,7 +1,7 @@
 /* SPDX-License-Identifier: Apache-2.0 */
 
 /*
- * Copyright 2018-2020 Joel E. Anderson
+ * Copyright 2018-2021 Joel E. Anderson
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -20,17 +20,8 @@
 #  define __STUMPLESS_PRIVATE_TARGET_H
 
 #  include <stddef.h>
-#  include <stumpless/config.h>
 #  include <stumpless/entry.h>
 #  include <stumpless/target.h>
-#  include "private/config/wrapper/thread_safety.h"
-
-#  ifdef STUMPLESS_THREAD_SAFETY_SUPPORTED
-#    define TARGET_MUTEX( TARGET ) \
-( ( config_mutex_t * ) ( ( char * ) ( TARGET ) + sizeof( *( TARGET ) ) ) )
-#  else
-#    define TARGET_MUTEX( TARGET ) NULL
-#  endif
 
 void
 destroy_target( const struct stumpless_target *target );
