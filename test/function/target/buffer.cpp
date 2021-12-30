e6282dd162f9dae558e3ea534c8567002bb6f6c4
Joel Anderson <joelanderson333@gmail.com>
2021-12-29 21:15:46 -0500

    fix buffer test memory leak

--- test/function/target/buffer.cpp
+++ test/function/target/buffer.cpp
@@ -280,9 +280,11 @@ namespace {
     const struct stumpless_error *error;
 
     target = stumpless_open_stdout_target( "not-a-buffer-target" );
-    stumpless_close_buffer_target( target );
 
+    stumpless_close_buffer_target( target );
     EXPECT_ERROR_ID_EQ( STUMPLESS_TARGET_INCOMPATIBLE );
+
+    stumpless_close_stream_target( target );
   }
 
   TEST( BufferTargetOpenTest, Basic ) {
