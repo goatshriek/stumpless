d5dbcdc8755e4ad436f6f1b46998236dc516eaea
Joel Anderson <joelanderson333@gmail.com>
2021-12-31 15:39:26 -0500

    fix some c++ leaks

--- test/function/cpp/element.cpp
+++ test/function/cpp/element.cpp
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: Apache-2.0
 
 /*
- * Copyright 2020 Joel E. Anderson
+ * Copyright 2020-2021 Joel E. Anderson
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -46,6 +46,8 @@ namespace {
 
   TEST_F( CppElementTest, GetParamByIndex ) {
      Param param = element_with_params->GetParam( ( size_t ) 0 );
-     EXPECT_STREQ( param.GetName(  ), param_1_name );
+     const char *param_name = param.GetName(  );
+     EXPECT_STREQ( param_name, param_1_name );
+     free( ( void * ) param_name );
   }
 }
