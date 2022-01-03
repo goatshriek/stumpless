81f697951f49644be4bf4c974e0261a2c9a3ed8e
Igor Kolesnik <igorkolesnik@posteo.net>
2022-01-03 20:56:08 +0100

    added regression tests

--- test/function/element.cpp
+++ test/function/element.cpp
@@ -1089,4 +1089,22 @@ namespace {
     EXPECT_NULL( result );
     EXPECT_ERROR_ID_EQ( STUMPLESS_ARGUMENT_EMPTY );
   }
+
+  TEST( AddParamTest, ParamNameTooLong ) {
+    struct stumpless_element *element;
+    struct stumpless_element *result;
+    const struct stumpless_error *error;
+
+    element = stumpless_new_element( "element" );
+
+    result = stumpless_add_new_param( element,
+				      "very-long-name-abcdefghijklmnopqrstuvwxyz",
+				      "test-value" );
+    EXPECT_NULL( result );
+    EXPECT_ERROR_ID_EQ( STUMPLESS_ARGUMENT_TOO_BIG );
+
+    stumpless_destroy_element_and_contents( element );
+    stumpless_free_all(  );
+  }
+
 }
