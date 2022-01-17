bbacc0e7aba47042876e3d29b4ecdb23655d7f01
Joel Anderson <joelanderson333@gmail.com>
2022-01-17 10:16:19 -0500

    increase journald search time

--- test/function/target/journald.cpp
+++ test/function/target/journald.cpp
@@ -28,7 +28,7 @@
 #include "test/helper/memory_allocation.hpp"
 
 #define FOR_JOURNALD_MATCH_BEGIN( MATCH )                   \
-for( int i = 0; i < 64 && !msg_found && !abort; i++ ) {     \
+for( int i = 0; i < 128 && !msg_found && !abort; i++ ) {    \
   result = sd_journal_open( &jrnl, SD_JOURNAL_LOCAL_ONLY ); \
   if( result < 0 ) {                                        \
     SUCCEED(  ) << "could not open the journal to verify "  \
