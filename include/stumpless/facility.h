dee6455b94aa0ed008cd5edb509138b50c3bd6cc
asinum <etienne.lafond@etu.u-paris.fr>
2022-05-19 20:32:39 +0200

    add stumpless_get_facility_string
    Signed-off-by: asinum <etienne.lafond@etu.u-paris.fr>

--- include/stumpless/facility.h
+++ include/stumpless/facility.h
@@ -347,6 +347,31 @@ enum stumpless_facility {
   STUMPLESS_FOREACH_FACILITY( STUMPLESS_GENERATE_ENUM )
 };
 
+  /**
+ * Gets the string representation of the given facility.
+ *
+ * This is a string literal that should not be modified or freed by the caller.
+ *
+ * **Thread Safety: MT-Safe**
+ * This function is thread safe.
+ *
+ * **Async Signal Safety: AS-Safe**
+ * This function is safe to call from signal handlers.
+ *
+ * **Async Cancel Safety: AC-Safe**
+ * This function is safe to call from threads that may be asynchronously
+ * cancelled.
+ *
+ * @param facility The facility to get the string from.
+ *
+ * @return The string representation of the given facility.
+ *
+ * @since release v2.1.0.
+ */
+STUMPLESS_PUBLIC_FUNCTION
+const char *
+stumpless_get_facility_string( enum stumpless_facility facility );
+
 #  ifdef __cplusplus
 } /* extern "C" */
 #  endif
