efb99b895e3260bb6289f98f985bc8fafdd917ef
Joel Anderson <joelanderson333@gmail.com>
2022-11-24 20:09:19 -0500

    remove redundant define

--- include/private/formatter.h
+++ include/private/formatter.h
@@ -1,7 +1,7 @@
 /* SPDX-License-Identifier: Apache-2.0 */
 
 /*
- * Copyright 2018-2020 Joel E. Anderson
+ * Copyright 2018-2022 Joel E. Anderson
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -27,7 +27,6 @@
 #  define RFC_5424_FULL_TIME_BUFFER_SIZE 10
 #  define RFC_5424_MAX_PRI_LENGTH 5
 #  define RFC_5424_MAX_TIMESTAMP_LENGTH 32
-#  define RFC_5424_MAX_HOSTNAME_LENGTH 255
 #  define RFC_5424_MAX_PROCID_LENGTH 128
 #  define RFC_5424_TIME_SECFRAC_BUFFER_SIZE 8
 #  define RFC_5424_TIMESTAMP_BUFFER_SIZE 33
