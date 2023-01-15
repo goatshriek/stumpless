ef6688290249de563a58342c4131a0865db517b6
Joel Anderson <joelanderson333@gmail.com>
2023-01-15 09:57:20 -0500

    update copyright years

--- test/function/target/udp6.cpp
+++ test/function/target/udp6.cpp
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: Apache-2.0
 
 /*
- * Copyright 2019-2021 Joel E. Anderson
+ * Copyright 2019-2023 Joel E. Anderson
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
