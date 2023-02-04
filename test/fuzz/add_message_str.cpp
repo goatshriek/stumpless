ca77148fdbe7794ca2e516a9b2fada76a5f3d4e2
Joel Anderson <joelanderson333@gmail.com>
2023-02-03 21:44:46 -0500

    add guard for zero size

--- test/fuzz/add_message_str.cpp
+++ test/fuzz/add_message_str.cpp
@@ -10,7 +10,9 @@ LLVMFuzzerTestOneInput( const uint8_t *data, size_t size ) {
   struct stumpless_target *target;
 
   memset( buffer, 0, sizeof( buffer ) );
-  data[size - 1] = '\0';
+  if( size > 0 ) {
+    data[size - 1] = '\0';
+  }
 
   target = stumpless_open_buffer_target( "fuzzer", buffer, sizeof( buffer ) );
   stumpless_add_message_str( target, ( char * ) data );
