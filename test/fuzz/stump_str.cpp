01fe6e91ffa972891aed4288bce2f854967ad896
angelart <angelart@umich.edu>
2023-04-13 19:32:59 -0400

    comment for target buffer stump_str fuzz test

--- test/fuzz/stump_str.cpp
+++ test/fuzz/stump_str.cpp
@@ -10,6 +10,7 @@ LLVMFuzzerTestOneInput( const uint8_t *data, size_t size ) {
   char buffer[8192];
   struct stumpless_target *target;
 
+  // Function will send message to the last opened target.
   target = stumpless_open_buffer_target( "fuzzer", buffer, sizeof( buffer ) );
   stump_str( fuzz_str.c_str(  ) );
   stumpless_close_buffer_target( target );
