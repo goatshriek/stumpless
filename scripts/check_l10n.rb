0f51ef89c50b7c8aefccee5110ae07412383d53f
Joel Anderson <joelanderson333@gmail.com>
2023-07-06 21:17:30 -0400

    update l10n regex to support string literal concatenation

--- scripts/check_l10n.rb
+++ scripts/check_l10n.rb
@@ -2,7 +2,7 @@
 
 # frozen_string_literal: true
 
-# Copyright 2022 Joel E. Anderson
+# Copyright 2022-2023 Joel E. Anderson
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -45,7 +45,7 @@ ARGV.each do |source_glob|
 
       todo = true if line.rstrip.end_with?('// todo translate')
 
-      str_match = line.match(/^L?"(.*)"/)
+      str_match = line.match(/^L?((("(.*)")|\w+| ))/)
       if current_l10n && str_match
         str << str_match[1]
 
