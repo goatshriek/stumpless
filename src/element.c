e89a1c9c8605871696ad4577b625f8a48fa63452
rmknan <rmknan@hotmail.com>
2023-09-25 17:52:37 -0400

    Valgrind test passing locally

--- src/element.c
+++ src/element.c
@@ -218,10 +218,10 @@ stumpless_element_to_string( const struct stumpless_element *element ) {
 
     if( param_count != 0 ) {
       // extra param list chars and commas
-      format_len += 2 + param_count - 1;
+      format_len += 4 + param_count ;
     } else {
       // no params, just name
-      format_len += 2;
+      format_len += 3;
     }
 
     format = alloc_mem( format_len );
@@ -254,7 +254,7 @@ stumpless_element_to_string( const struct stumpless_element *element ) {
     } else {
       // name (no params)
       // pos_offset is name_len + 2 here
-      pos_offset -= 2;
+      pos_offset -= 3;
     }
 
     format[pos_offset + 1] = '\0';
