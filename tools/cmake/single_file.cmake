ffb25bc0acf4e508f488c74d67594480fa33c66a
Joel Anderson <joelanderson333@gmail.com>
2023-09-29 17:28:10 -0400

    move single file build to dedicated file

---
+++ tools/cmake/single_file.cmake
@@ -0,0 +1,55 @@
+set(SINGLE_SOURCE_FILE "${PROJECT_BINARY_DIR}/stumpless.c")
+message("creating single source library ${SINGLE_SOURCE_FILE}")
+
+function(include_file source_filename include_filenames already_included)
+  foreach(include_filename ${include_filenames})
+    file(APPEND "${source_filename}" "\n/* ${include_filename} */\n\n")
+    file(STRINGS "${include_filename}" raw_include_contents NEWLINE_CONSUME ENCODING UTF-8)
+    string(REPLACE "\;" "@SEMICOLON@" sanitized_include_contents "${raw_include_contents}")
+    string(REPLACE "[" "@OPEN_BRACKET@" sanitized_include_contents "${sanitized_include_contents}")
+    string(REPLACE "]" "@CLOSE_BRACKET@" sanitized_include_contents "${sanitized_include_contents}")
+    string(REPLACE "\n" ";" file_lines "${sanitized_include_contents}")
+    foreach(line ${file_lines})
+      if("${line}" MATCHES ".*include \"(private/.+\.h)\"$")
+        if("${CMAKE_MATCH_1}" STREQUAL "private/config.h")
+          set(extracted_full_path "${PROJECT_BINARY_DIR}/include/private/config.h")
+        else()
+          set(extracted_full_path "${PROJECT_SOURCE_DIR}/include/${CMAKE_MATCH_1}")
+        endif()
+        list(FIND ${already_included} "${extracted_full_path}" FOUND_INDEX)
+        if(${FOUND_INDEX} EQUAL -1)
+          list(APPEND ${already_included} "${extracted_full_path}")
+          include_file(
+            "${source_filename}"
+            "${extracted_full_path}"
+            ${already_included}
+          )
+        endif()
+      else()
+        set(prev_line "")
+        foreach(subline ${line})
+          if(NOT prev_line STREQUAL "")
+            file(APPEND "${source_filename}" "${prev_line}\\\n")
+          endif()
+          string(REPLACE "@CLOSE_BRACKET@" "]" original_subline "${subline}")
+          string(REPLACE "@OPEN_BRACKET@" "[" original_subline "${original_subline}")
+          string(REPLACE "@SEMICOLON@" ";" original_subline "${original_subline}")
+          set(prev_line "${original_subline}")
+        endforeach()
+        file(APPEND "${source_filename}" "${prev_line}\n")
+      endif()
+    endforeach()
+  endforeach()
+endfunction()
+
+file(WRITE "${SINGLE_SOURCE_FILE}" "")
+set(include_list "")
+foreach(source_file ${STUMPLESS_SOURCES})
+  if("${source_file}" MATCHES "\\.c$")
+    include_file(
+      "${SINGLE_SOURCE_FILE}"
+      "${source_file}"
+      include_list
+    )
+  endif()
+endforeach()
