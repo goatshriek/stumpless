c64fa0686947d85b4d5a60f40d14676f3db54de0
Joel Anderson <joel@goatshriek.com>
2024-07-30 09:06:27 -0400

    update gtest

--- tools/cmake/gtest.cmake
+++ tools/cmake/gtest.cmake
@@ -66,7 +66,7 @@ if(${gtest_lib} STREQUAL "gtest_lib-NOTFOUND" OR ${gtest_main_lib} STREQUAL "gte
   )
 
   ExternalProject_Add(gtest
-    URL https://github.com/google/googletest/archive/44c03643cfbc649488a0f437cd18e05f11960d19.zip
+    URL https://github.com/google/googletest/archive/e39786088138f2749d64e9e90e0f9902daa77c40.zip
     PREFIX ${CMAKE_CURRENT_BINARY_DIR}/gtest
     CMAKE_ARGS -Dgtest_force_shared_crt=ON -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE} -DCMAKE_SH=${CMAKE_SH} -DBUILD_SHARED_LIBS=ON -DCMAKE_CXX_FLAGS=${google_libs_cxx_flags}
     UPDATE_COMMAND ""
