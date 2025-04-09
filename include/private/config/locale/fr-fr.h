db6a9f769b8f405e842b6c2c720bd2bc8d563404
DevTigerart <dev.tigerart@gmail.com>
2025-04-09 08:57:18 +0200

    Update fr-fr localization

--- include/private/config/locale/fr-fr.h
+++ include/private/config/locale/fr-fr.h
@@ -22,16 +22,14 @@
 #  define L10N_BIND_UNIX_SOCKET_FAILED_ERROR_MESSAGE \
 "impossible de se lier au socket local unix"
 
-// todo translate
 #  define L10N_BUFFER_SIZE_ERROR_CODE_TYPE \
-"buffer used to store the message"
+"le tampon utilisé pour stocker le message"
 
 #  define L10N_BUFFER_TOO_SMALL_ERROR_MESSAGE \
 "le tampon est trop petit pour le message donné"
 
-// todo translate
 #  define L10N_CHAIN_TARGETS_UNSUPPORTED \
-"chain targets are not supported by this build"
+"les cibles chaînées ne sont pas supportées par ce build"
 
 #  define L10N_CLOSE_UNSUPPORTED_TARGET_ERROR_MESSAGE \
 "tentative de fermeture d'un type de cible no supporté"
@@ -115,9 +113,8 @@
 #  define L10N_INVALID_MULTI_SZ_ERROR_MESSAGE \
 "la valeur d'un registre MULTI_SZ n'était ni vide ni terminée par deux NULL"
 
-// todo translate
 #  define L10N_INVALID_PARAM_ERROR_MESSAGE \
-"the string is not of the name=\"value\" format"
+"la chaîne de caractères n'est pas au format nom=\"valeur\""
 
 #  define L10N_INVALID_SEVERITY_ERROR_CODE_TYPE \
 "la sévérité invalide"
@@ -163,15 +160,14 @@
 #  define L10N_NAME_RESOLUTION_FAILED_ERROR_MESSAGE \
 "gethostbyname[2] et inet_pton échec de résolvez le nom"
 
-// todo translate
 #  define L10N_NETWORK_CLOSED_ERROR_MESSAGE \
-"the network connection is closed"
+"la connection réseau est fermée"
 
 #  define L10N_NETWORK_PROTOCOL_UNSUPPORTED_ERROR_MESSAGE \
-"le protocol réseaux choisi n'est pas supporté"
+"le protocol réseau choisi n'est pas supporté"
 
 #  define L10N_NETWORK_TARGETS_UNSUPPORTED \
-"les cibles réseaux ne sont pas supportés par cette build"
+"les cibles réseaux ne sont pas supportés par ce build"
 
 #  define L10N_NULL_ARG_ERROR_MESSAGE( ARG_NAME ) \
 ARG_NAME " a été NULL"
@@ -212,9 +208,8 @@ ARG_NAME " a été NULL"
 #  define L10N_SEND_WIN_SOCKET_FAILED_ERROR_MESSAGE \
 "envoi échoué avec un socket winsock2"
 
-// todo translate
 #  define L10N_SEVERITY_COLORS_UNSUPPORTED_TARGET_ERROR_MESSAGE \
-"severity colors are only supported for stream targets"
+"les couleurs de sévérité sont uniquement supportées pour les cibles de type stream"
 
 #  define L10N_SOCKET_FAILED_ERROR_MESSAGE \
 "échec d'ouverture d'un socket"
@@ -222,45 +217,36 @@ ARG_NAME " a été NULL"
 #  define L10N_SOURCE_REGISTRATION_TRANSACTION_DESCRIPTION_W \
 L"enregistrement Stumpless de Windows Event Log Source"
 
-// todo translate
 #  define L10N_SQLITE3_BIND_FAILED_ERROR_MESSAGE( PARAM )  \
-"could not bind " PARAM " to the statement"
+"n'a pas pu lier " PARAM " à la déclaration"
 
-// todo translate
 #  define L10N_SQLITE3_BUSY_ERROR_MESSAGE \
-"the database was busy and could not complete the transaction"
+"la base de données était occupée et n'a pas pu terminer la transaction"
 
-// todo translate
 #  define L10N_SQLITE3_CLOSE_FAILED_ERROR_MESSAGE \
-"could not close the sqlite3 database"
+"n'a pas pu fermer la base de données sqlite3"
 
 // todo translate
 #  define L10N_SQLITE3_CUSTOM_PREPARE_FAILED_ERROR_MESSAGE \
-"a custom callback for prepared statements failed"
+"une callback personnalisée pour la préparation a échoué"
 
-// todo translate
 #  define L10N_SQLITE3_OPEN_FAILED_ERROR_MESSAGE \
-"could not open the sqlite3 database"
+"n'a pas pu ouvrir la base de données sqlite3"
 
-// todo translate
 #  define L10N_SQLITE3_PREPARE_FAILED_ERROR_MESSAGE \
-"sqlite3_prepare_v2 failed"
+"sqlite3_prepare_v2 a échoué"
 
-// todo translate
 #  define L10N_SQLITE3_RESULT_CODE_TYPE \
-"the return code of the failed sqlite3 call"
+"le code de retour de l'appel sqlite3 qui a échoue"
 
-// todo translate
 #  define L10N_SQLITE3_RETRY_COUNT_CODE_TYPE \
-"the number of times the operation was retried"
+"le nombre de fois que l'opération a été tenté"
 
-// todo translate
 #  define L10N_SQLITE3_STEP_FAILED_ERROR_MESSAGE \
-"sqlite3_step failed"
+"sqlite3_step a échoué"
 
-// todo translate
 #  define L10N_SQLITE3_TARGETS_UNSUPPORTED \
-"sqlite3 targets are not supported by this build"
+"les cibles sqlite3 ne sont pas supportées par ce build"
 
 #  define L10N_STREAM_WRITE_FAILURE_ERROR_MESSAGE \
 "impossible d'écrire dans le flux"
