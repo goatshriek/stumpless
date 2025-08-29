6a1e8b96c6b7c519841de8dcba734fb7fdac2fef
Joel <joel@goatshriek.com>
2025-08-28 20:21:18 -0400

    close tcp listener after closing network target
    The network target thread safety test
    Tcp4WriteConsistency.SimultaneousWrites would occasionally hang in MacOS
    Github Actions runs. Testing with FreeBSD revealed that the join of the
    listening threads needed to be done after closing the network targets
    in order for the connection to properly terminate.

--- test/thread_safety/target/network.cpp
+++ test/thread_safety/target/network.cpp
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: Apache-2.0
 
 /*
- * Copyright 2020 Joel E. Anderson
+ * Copyright 2020-2025 Joel E. Anderson
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -163,8 +163,8 @@ namespace {
                                 false );
 
       // cleanup after the test
-      listener_thread->join();
       stumpless_close_network_target( target );
+      listener_thread->join();
       EXPECT_NO_ERROR;
       stumpless_free_all(  );
 
