1819cccec60d931a6c03c79b49cf441023f0e296
naoto <naotofujiwara890@gmail.com>
2025-09-11 17:19:03 +0900

    modified sk-sk

--- include/private/config/locale/sk-sk.h
+++ include/private/config/locale/sk-sk.h
@@ -141,6 +141,13 @@
 "kód služby musí byť definovaný v súlade s normou RFC 5424: hodnoty medzi" \
 " 0 a 7 vrátane"
 
+# define L10N_INVALID_PRIVAL_ERROR_CODE_TYPE \
+"neplatné prival"
+
+#define L10N_INVALID_PRIVAL_ERROR_MESSAGE \
+"kód prival musí byť definovaný v súlade s normou RFC 5424: hodnoty medzi" \
+" 0 a 255 vrátane"
+
 // todo translate
 #  define L10N_INVALID_STATE_DURING_UTF8_PARSING \
 "invalid state reached during UTF-8 string parsing"
