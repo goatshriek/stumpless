f335b4ef1c2f2b911b48b486f9ddd3aaa86d8c80
naoto <naotofujiwara890@gmail.com>
2025-09-11 17:20:37 +0900

    modified pt-br

--- include/private/config/locale/pt-br.h
+++ include/private/config/locale/pt-br.h
@@ -126,6 +126,13 @@
 "códigos de gravidade devem ser definidos de acordo com a RFC 5424:" \
 " valores entre 0 e 7, incluso"
 
+# define L10N_INVALID_PRIVAL_ERROR_CODE_TYPE \
+"o prival inválido"
+
+#define L10N_INVALID_PRIVAL_ERROR_MESSAGE \
+"códigos prival devem ser definidos de acordo com a RFC 5424: valores entre 0" \
+" e 255, inclusive"
+
 #  define L10N_INVALID_STATE_DURING_UTF8_PARSING \
 "Estado inválido na leitura da string UTF-8"
 
