d0aab20431017865433430a3c201050600d90f83
naoto <naotofujiwara890@gmail.com>
2025-09-11 17:13:37 +0900

    modifed fr-fr

--- include/private/config/locale/fr-fr.h
+++ include/private/config/locale/fr-fr.h
@@ -123,6 +123,13 @@
 "les codes de sévérité doivent être définis conformément au RFC 5424:" \
 " valeurs entre 0 et 7 inclus"
 
+# define L10N_INVALID_PRIVAL_ERROR_CODE_TYPE \
+"le prival invalide"
+
+#define L10N_INVALID_PRIVAL_ERROR_MESSAGE \
+"les codes prival doivent être définis conformément au RFC 5424 : valeurs" \
+" comprises entre 0 et 255 inclus"
+
 #  define L10N_INVALID_STATE_DURING_UTF8_PARSING \
 "état invalide atteint pendant UTF-8 string parsing"
 
