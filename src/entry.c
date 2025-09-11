a3eb4e5db2c4f6b6db435a78e2ff9676237af73c
naoto <naotofujiwara890@gmail.com>
2025-09-11 15:17:06 +0900

    add validation to stumpless_set_entry_prival

--- src/entry.c
+++ src/entry.c
@@ -1026,6 +1026,9 @@ stumpless_set_entry_priority( struct stumpless_entry *entry,
 struct stumpless_entry *
 stumpless_set_entry_prival( struct stumpless_entry *entry,
                             int prival ) {
+  if ((prival & 0xFF) != prival)
+    return NULL;
+  
   return stumpless_set_entry_priority( entry,
                                        get_facility( prival ),
                                        get_severity( prival ) );
