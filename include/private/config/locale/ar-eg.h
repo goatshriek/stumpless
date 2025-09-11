b3b9b1d0b0f163ceb1a476eafc856c59e4b65905
naoto <naotofujiwara890@gmail.com>
2025-09-11 17:06:49 +0900

    modifed ar

--- include/private/config/locale/ar-eg.h
+++ include/private/config/locale/ar-eg.h
@@ -149,6 +149,12 @@
     "قيمة الشدة يجب ان تكون بصيغة"          \
     "RFC 5424: القيم بين 0 و 7 شاملة"
 
+# define L10N_INVALID_PRIVAL_ERROR_CODE_TYPE \
+"قيمة الخطورة والمنشأة غير صالحة"
+
+#define L10N_INVALID_PRIVAL_ERROR_MESSAGE \
+"قيمة الخطورة والمنشأة يجب ان تكون بصيغة RFC 5424: القيم بين 0 و 255 شاملة"
+
 #define L10N_INVALID_STATE_DURING_UTF8_PARSING   \
     "الوصول الي حالة غير صالحة اثناء تحليل النص" \
     "UTF-8"
