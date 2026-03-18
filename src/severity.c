429d23462a4cf58fa903a278bd76177a72d414ca
Lemuren <h.e.thorsell@gmail.com>
2026-03-18 12:11:04 +0000

    ensure partial buffers don't match

--- src/severity.c
+++ src/severity.c
@@ -60,20 +60,24 @@ enum stumpless_severity stumpless_get_severity_enum_from_buffer(const char *seve
                      sizeof( severity_enum_to_string[0] );
 
   for( i = 0; i < severity_bound; i++ ) {
-    if( config_strncasecmp( severity_buffer, severity_enum_to_string[i] + str_offset, severity_buffer_length ) == 0 ) {
+    const char *target = severity_enum_to_string[i] + str_offset;
+    if( (config_strncasecmp( severity_buffer, target, severity_buffer_length ) == 0) && (strlen(target) == severity_buffer_length) ) {
       return i;
     }
   }
 
-  if( config_strncasecmp( severity_buffer, "PANIC", severity_buffer_length ) == 0 ) {
+  if( (config_strncasecmp( severity_buffer, "PANIC", severity_buffer_length ) == 0) &&
+      (severity_buffer_length == 5) ) {
     return STUMPLESS_SEVERITY_EMERG_VALUE;
   }
 
-  if( config_strncasecmp( severity_buffer, "ERROR", severity_buffer_length ) == 0 ) {
+  if( (config_strncasecmp( severity_buffer, "ERROR", severity_buffer_length ) == 0) &&
+      (severity_buffer_length == 5) ) {
     return STUMPLESS_SEVERITY_ERR_VALUE;
   }
 
-  if( config_strncasecmp( severity_buffer, "WARN", severity_buffer_length ) == 0 ) {
+  if( (config_strncasecmp( severity_buffer, "WARN", severity_buffer_length ) == 0) &&
+      (severity_buffer_length == 4) ) {
     return STUMPLESS_SEVERITY_WARNING_VALUE;
   }
 
